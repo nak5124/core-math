16da72cc35bfd29d67c0560f10fb2b99592fe8d5
Stephane Glondu <steph@glondu.net>
2022-03-21 15:34:07 +0100

    More generic perf.sh script instead of throughput.sh

---
+++ perf.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Usage: ./perf.sh acos
+
+f=$1
+u="$(echo src/binary*/*/$f.c)"
+
+if [ -f "$u" ]; then
+    dir="${u%/*}"
+else
+    echo "Unknown function: $f"
+    exit 2
+fi
+
+export CFLAGS="-O3 -march=native"
+
+cd $dir
+make -s clean
+make -s perf
+./perf --file /tmp/randoms.dat --reference --count 1000000
+perf stat -e cpu-cycles --no-big-num -x';' ./perf --file /tmp/randoms.dat --count 1000000 --repeat 1000 $PERF_ARGS 2>&1 | { IFS=';' read a b; printf 'scale=3\n%s/1000000000\n' $a | bc; }
+perf stat -e cpu-cycles --no-big-num -x';' ./perf --file /tmp/randoms.dat --count 1000000 --repeat 1000 --libc $PERF_ARGS 2>&1 | { IFS=';' read a b; printf 'scale=3\n%s/1000000000\n' $a | bc; }
+
+if [ -n "$LIBM" ]; then
+    unset LIBM
+    make -s clean
+    make -s perf
+    perf stat -e cpu-cycles --no-big-num -x';' ./perf --file /tmp/randoms.dat --count 1000000 --repeat 1000 --libc $PERF_ARGS 2>&1 | { IFS=';' read a b; printf 'scale=3\n%s/1000000000\n' $a | bc; }
+fi
--- throughput.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Usage: ./throughput.sh acos
-f=$1
-cd src/binary32/$f
-make -s clean
-make -s CFLAGS="-O3 -march=native"
-./perf --file /tmp/randoms.dat --reference --count 1000000
-perf stat -e cpu-cycles --no-big-num -x';' ./perf --file /tmp/randoms.dat --count 1000000 --repeat 1000 2>&1 | { IFS=';' read a b; printf 'scale=3\n%s/1000000000\n' $a | bc; }
