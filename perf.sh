46969069249b38c72410bd2996592f65fb0f696d
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2022-04-13 19:59:07 +0200

    added -ffinite-math-only to CFLAGS in perf.sh

--- perf.sh
+++ perf.sh
@@ -12,8 +12,9 @@ else
 fi
 
 # for clang we might want to add -ffp-contract=on to enable FMA
+# -ffinite-math-only is needed to inline fmaxf and fminf
 if [ "$CFLAGS" == "" ]; then
-   export CFLAGS="-O3 -march=native"
+   export CFLAGS="-O3 -march=native -ffinite-math-only"
 fi
 
 if [ -n "$LIBM" ]; then
