c4b74e9ddcab3d7a9194d6803d3f7f0ce8f8a9c9
Stephane Glondu <steph@glondu.net>
2022-04-20 08:57:32 +0200

    Add support for CORE_MATH_PERF_MODE in perf.sh

--- perf.sh
+++ perf.sh
@@ -4,6 +4,11 @@
 f=$1
 u="$(echo src/binary*/*/$f.c)"
 
+if [ -z "$CORE_MATH_PERF_MODE" ]; then
+    echo 'Please set CORE_MATH_PERF_MODE environment variable (perf or rdtsc)'
+    exit 2
+fi
+
 if [ -f "$u" ]; then
     dir="${u%/*}"
 else
@@ -26,12 +31,22 @@ cd $dir
 make -s clean
 make -s perf
 ./perf --file /tmp/randoms.dat --reference --count 1000000
-perf stat -e cpu-cycles --no-big-num -x';' ./perf --file /tmp/randoms.dat --count 1000000 --repeat 1000 $PERF_ARGS 2>&1 | { IFS=';' read a b; printf 'scale=3\n%s/1000000000\n' $a | bc; }
-perf stat -e cpu-cycles --no-big-num -x';' ./perf --file /tmp/randoms.dat --count 1000000 --repeat 1000 --libc $PERF_ARGS 2>&1 | { IFS=';' read a b; printf 'scale=3\n%s/1000000000\n' $a | bc; }
+
+if [ "$CORE_MATH_PERF_MODE" = perf ]; then
+    perf stat -e cpu-cycles --no-big-num -x';' ./perf --file /tmp/randoms.dat --count 1000000 --repeat 1000 $PERF_ARGS 2>&1 | { IFS=';' read a b; printf 'scale=3\n%s/1000000000\n' $a | bc; }
+    perf stat -e cpu-cycles --no-big-num -x';' ./perf --file /tmp/randoms.dat --count 1000000 --repeat 1000 --libc $PERF_ARGS 2>&1 | { IFS=';' read a b; printf 'scale=3\n%s/1000000000\n' $a | bc; }
+elif [ "$CORE_MATH_PERF_MODE" = rdtsc ]; then
+    ./perf --file /tmp/randoms.dat --count 1000000 --repeat 1000 $PERF_ARGS --rdtsc
+    ./perf --file /tmp/randoms.dat --count 1000000 --repeat 1000 --libc $PERF_ARGS --rdtsc
+fi
 
 if [ -n "$BACKUP_LIBM" ]; then
     export LIBM="$BACKUP_LIBM"
     make -s clean
     make -s perf
-    perf stat -e cpu-cycles --no-big-num -x';' ./perf --file /tmp/randoms.dat --count 1000000 --repeat 1000 --libc $PERF_ARGS 2>&1 | { IFS=';' read a b; printf 'scale=3\n%s/1000000000\n' $a | bc; }
+    if [ "$CORE_MATH_PERF_MODE" = perf ]; then
+        perf stat -e cpu-cycles --no-big-num -x';' ./perf --file /tmp/randoms.dat --count 1000000 --repeat 1000 --libc $PERF_ARGS 2>&1 | { IFS=';' read a b; printf 'scale=3\n%s/1000000000\n' $a | bc; }
+    elif [ "$CORE_MATH_PERF_MODE" = rdtsc ]; then
+        ./perf --file /tmp/randoms.dat --count 1000000 --repeat 1000 --libc $PERF_ARGS --rdtsc
+    fi
 fi
