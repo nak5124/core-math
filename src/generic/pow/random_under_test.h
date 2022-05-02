d14da3d7f2fc6454f36dd58b517241141ec351d7
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2022-05-02 16:01:44 +0200

    for pow, use random numbers in [0,10] instead of [-10,10]

--- src/generic/pow/random_under_test.h
+++ src/generic/pow/random_under_test.h
@@ -1,5 +1,5 @@
 static inline TYPE_UNDER_TEST random_under_test (void)
 {
-  /* sample in [-10,10] */
-  return 20 * ((TYPE_UNDER_TEST) rand() / (TYPE_UNDER_TEST) RAND_MAX) - 10;
+  /* sample in [0,10] since x^y gives NaN for x negative and y non-integer */
+  return 10 * ((TYPE_UNDER_TEST) rand() / (TYPE_UNDER_TEST) RAND_MAX);
 }
