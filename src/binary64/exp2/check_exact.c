6386f2b248833cc7599ac851a72a02f547a731a2
Stéphane Glondu <steph@glondu.net>
2022-06-29 10:42:15 +0200

    Add "./check.sh --exact exp2"

---
+++ src/binary64/exp2/check_exact.c
@@ -0,0 +1,94 @@
+/* Generate exact cases for exp2 testing.
+
+Copyright (c) 2022 Stéphane Glondu and Paul Zimmermann, Inria.
+
+This file is part of the CORE-MATH project
+(https://core-math.gitlabpages.inria.fr/).
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fenv.h>
+#include <math.h>
+
+double cr_exp2 (double);
+
+int rnd1[] = { FE_TONEAREST, FE_TOWARDZERO, FE_UPWARD, FE_DOWNWARD };
+
+int rnd = 0;
+int verbose = 0;
+
+int
+main (int argc, char *argv[])
+{
+  while (argc >= 2)
+    {
+      if (strcmp (argv[1], "--rndn") == 0)
+        {
+          rnd = 0;
+          argc --;
+          argv ++;
+        }
+      else if (strcmp (argv[1], "--rndz") == 0)
+        {
+          rnd = 1;
+          argc --;
+          argv ++;
+        }
+      else if (strcmp (argv[1], "--rndu") == 0)
+        {
+          rnd = 2;
+          argc --;
+          argv ++;
+        }
+      else if (strcmp (argv[1], "--rndd") == 0)
+        {
+          rnd = 3;
+          argc --;
+          argv ++;
+        }
+      else if (strcmp (argv[1], "--verbose") == 0)
+        {
+          verbose = 1;
+          argc --;
+          argv ++;
+        }
+      else
+        {
+          fprintf (stderr, "Error, unknown option %s\n", argv[1]);
+          exit (1);
+        }
+    }
+
+  int ret = 0;
+  for (int i = -1074; i <= 1023; i++) {
+    double x = i;
+    feclearexcept(FE_INEXACT);
+    cr_exp2(x);
+    if (fetestexcept(FE_INEXACT)) {
+      printf ("FAIL i=%d, x=%la\n", i, x);
+      fflush (stdout);
+      ret = 1;
+    }
+  }
+  return ret;
+}
