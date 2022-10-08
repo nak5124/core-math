b5612ff9ed9105f88fd335db9e26c8e2860efcc5
Alexei Sibidanov <sibid@uvic.ca>
2022-10-08 13:39:47 -0700

    first implementation of reciprocal square root in binary32

---
+++ src/binary32/rsqrt/function_under_test.h
@@ -0,0 +1,17 @@
+#define cr_function_under_test cr_rsqrtf
+#define ref_function_under_test ref_rsqrt
+
+void doit (uint32_t n);
+static inline uint32_t asuint (float f);
+
+static inline int doloop (void)
+{
+  uint32_t nmin = asuint (0x0p0f), nmax = asuint (0x1.fffffep127f);
+#pragma omp parallel for schedule(dynamic,1024)
+  for (uint32_t n = nmin; n <= nmax; n++)
+  {
+    doit (n);
+  }
+  printf ("all ok\n");
+  return 0;
+}
