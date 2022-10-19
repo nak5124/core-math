23fac5677b7309d15d2218f2a9005183474b5346
Stéphane Glondu <steph@glondu.net>
2022-10-19 09:05:19 +0200

    Add an helper script to call `perf annonate`

---
+++ prof.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Usage: ./prof.sh acos
+
+RANDOMS_FILE="$(mktemp /tmp/core-math.XXXXXX)"
+PERF_DATA_FILE="$(mktemp /tmp/core-math.XXXXXX)"
+trap "rm -f $RANDOMS_FILE $PERF_DATA_FILE" 0
+
+f=$1
+u="$(echo src/binary*/*/$f.c)"
+
+if [ -f "$u" ]; then
+    dir="${u%/*}"
+else
+    echo "Unknown function: $f"
+    exit 2
+fi
+
+# for clang we might want to add -ffp-contract=on to enable FMA
+# -ffinite-math-only is needed to inline fmaxf and fminf
+if [ "$CFLAGS" == "" ]; then
+   export CFLAGS="-O3 -march=native -ffinite-math-only"
+fi
+
+CFLAGS="$CFLAGS -ggdb"
+
+COMMON_PERF_ARGS=(--file "$RANDOMS_FILE" --count 1000000)
+
+cd $dir
+make -s clean
+make -s perf
+./perf "${COMMON_PERF_ARGS[@]}" --reference
+
+perf record --output="$PERF_DATA_FILE" ./perf "${COMMON_PERF_ARGS[@]}" --repeat 500
+perf annotate --input="$PERF_DATA_FILE"
