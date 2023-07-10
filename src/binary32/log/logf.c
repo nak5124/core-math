07b4664f7d2209b2558c7eb1843571c4e32822c0
Alexei Sibidanov <sibid@uvic.ca>
2023-07-09 17:20:01 -0700

    further reduce latency with a smaller degree polynomial

--- src/binary32/log/logf.c
+++ src/binary32/log/logf.c
@@ -66,25 +66,24 @@ float cr_logf(float x){
     0x1.1111111p-1, 0x1.0ecf56cp-1, 0x1.0c9715p-1, 0x1.0a6810ap-1,
     0x1.0842108p-1, 0x1.0624dd3p-1, 0x1.041041p-1, 0x1.0204081p-1, 0.5};
   static const double tl[] = {
-    0x1.949eca9b20079p-37, 0x1.fc0a891612b97p-7, 0x1.f829b1eaac6dap-6, 0x1.77458f54c26e9p-5,
-    0x1.f0a30c22aac93p-5, 0x1.341d793c876c7p-4, 0x1.6f0d28d321042p-4, 0x1.a926d3a777a5ap-4,
-    0x1.e2707723797dcp-4, 0x1.0d77e7a96e0d5p-3, 0x1.29552f706479ep-3, 0x1.44d2b6c61cf9ap-3,
-    0x1.5ff306eede64fp-3, 0x1.7ab8904172b84p-3, 0x1.9525a9e3aa93p-3, 0x1.af3c94ed7126ep-3,
-    0x1.c8ff7c5a0ec9dp-3, 0x1.e27076d614561p-3, 0x1.fb9186b6490a6p-3, 0x1.0a324e38eba21p-2,
-    0x1.1675cacaecf4cp-2, 0x1.22941fc12a2a3p-2, 0x1.2e8e2bc34466ep-2, 0x1.3a64c56b46f27p-2,
-    0x1.4618bc31f88p-2, 0x1.51aad8751216bp-2, 0x1.5d1bdbeab3308p-2, 0x1.686c81d363decp-2,
-    0x1.739d7f6def944p-2, 0x1.7eaf83c85d901p-2, 0x1.89a3385846b98p-2, 0x1.947941aac4038p-2,
-    0x1.9f323edc2c18ap-2, 0x1.a9cec9a453187p-2, 0x1.b44f77c5fb8ap-2, 0x1.beb4d9eaa44bap-2,
-    0x1.c8ff7c69dc35fp-2, 0x1.d32fe7f3c1513p-2, 0x1.dd46a0504eddep-2, 0x1.e74426181b0c5p-2,
-    0x1.f128f5eb2302ap-2, 0x1.faf588ddc1c5cp-2, 0x1.02552a5ef659ep-1, 0x1.0723e5c6673dfp-1,
-    0x1.0be72e386bf22p-1, 0x1.109f39d56e136p-1, 0x1.154c3d2c66a88p-1, 0x1.19ee6b38d5e0ep-1,
-    0x1.1e85f5ef1d56fp-1, 0x1.23130d7fc53e1p-1, 0x1.2795e121b45bap-1, 0x1.2c0e9ec9e232bp-1,
-    0x1.307d73380a55dp-1, 0x1.34e289cb67672p-1, 0x1.393e0d42fbeb8p-1, 0x1.3d9026ad6eb99p-1,
-    0x1.41d8fe868074dp-1, 0x1.4618bc1edf361p-1, 0x1.4a4f85d31d35ap-1, 0x1.4e7d81280f04fp-1,
-    0x1.52a2d26dd5a4ap-1, 0x1.56bf9d5998838p-1, 0x1.5ad404cb733ccp-1, 0x1.5ee02a929ab14p-1,
-    0x1.62e42fefbce8ep-1};
-  static const double b[] =
-    {0x1.ffffffdffdfebp-1, -0x1.ffffff754925cp-2, 0x1.556556159ed6ap-2, -0x1.0012abdb2e5cp-2};
+    -0x1.3b40815cd0628p-45, 0x1.fc0a890fbb514p-7, 0x1.f829b1e780b98p-6, 0x1.77458f532c948p-5,
+    0x1.f0a30c2114ef2p-5, 0x1.341d793bbc7f7p-4, 0x1.6f0d28d256172p-4, 0x1.a926d3a6acb89p-4,
+    0x1.e2707722ae90cp-4, 0x1.0d77e7a90896cp-3, 0x1.29552f6fff036p-3, 0x1.44d2b6c5b7831p-3,
+    0x1.5ff306ee78ee7p-3, 0x1.7ab890410d41cp-3, 0x1.9525a9e3451c7p-3, 0x1.af3c94ed0bb06p-3,
+    0x1.c8ff7c59a9535p-3, 0x1.e27076d5aedf9p-3, 0x1.fb9186b5e393ep-3, 0x1.0a324e38b8e6dp-2,
+    0x1.1675cacaba398p-2, 0x1.22941fc0f76efp-2, 0x1.2e8e2bc311abap-2, 0x1.3a64c56b14373p-2,
+    0x1.4618bc31c5c4cp-2, 0x1.51aad874df5b7p-2, 0x1.5d1bdbea80754p-2, 0x1.686c81d331238p-2,
+    0x1.739d7f6dbcd9p-2, 0x1.7eaf83c82ad4dp-2, 0x1.89a3385813fe4p-2, 0x1.947941aa91484p-2,
+    0x1.9f323edbf95d5p-2, 0x1.a9cec9a4205d3p-2, 0x1.b44f77c5c8cecp-2, 0x1.beb4d9ea71905p-2,
+    0x1.c8ff7c69a97abp-2, 0x1.d32fe7f38e95fp-2, 0x1.dd46a0501c22ap-2, 0x1.e7442617e8511p-2,
+    0x1.f128f5eaf0476p-2, 0x1.faf588dd8f0a8p-2, 0x1.02552a5edcfc4p-1, 0x1.0723e5c64de05p-1,
+    0x1.0be72e3852947p-1, 0x1.109f39d554b5cp-1, 0x1.154c3d2c4d4aep-1, 0x1.19ee6b38bc834p-1,
+    0x1.1e85f5ef03f95p-1, 0x1.23130d7fabe07p-1, 0x1.2795e1219afep-1, 0x1.2c0e9ec9c8d5p-1,
+    0x1.307d7337f0f83p-1, 0x1.34e289cb4e098p-1, 0x1.393e0d42e28dep-1, 0x1.3d9026ad555bfp-1,
+    0x1.41d8fe8667173p-1, 0x1.4618bc1ec5d87p-1, 0x1.4a4f85d303d8p-1, 0x1.4e7d8127f5a75p-1,
+    0x1.52a2d26dbc47p-1, 0x1.56bf9d597f25ep-1, 0x1.5ad404cb59df2p-1, 0x1.5ee02a928153ap-1,
+    0x1.62e42fefa38b4p-1};
+  static const double b[] = {0x1.00000006342eap+0, -0x1.0001f7fdc3977p-1, 0x1.554a4e5cae9cfp-2};
   static const double c[] =
     {-0x1p-1, 0x1.55555555571cap-2, -0x1.0000000002d85p-2, 0x1.9999987d0c963p-3,
      -0x1.555554059a8bbp-3, 0x1.24aebcf71a38fp-3, -0x1.001c73915d758p-3};
@@ -101,8 +100,8 @@ float cr_logf(float x){
   int32_t e = ((int32_t)ux>>23)-127;
   b64u64_u tz = {.u = ((uint64_t)m|(1023l<<23))<<(52-23)};
   double z = tz.f*tr[j] - 1, z2 = z*z;
-  double r = (e*0x1.62e42fefa39efp-1 + tl[j]) + z*((b[0] + z*b[1]) + z2*(b[2] + z*b[3]));
-  float ub = r, lb = r - 0x1.99e6p-36;
+  double r = ((e*0x1.62e42fefa39efp-1 + tl[j]) + z*b[0]) + z2*(b[1] + z*b[2]);
+  float ub = r, lb = r + 0x1.f06p-33;
   if(__builtin_expect(ub != lb, 0)){
     double f = z2*((c[0] + z*c[1]) + z2*((c[2] + z*c[3]) + z2*(c[4] + z*c[5] + z2*c[6])));
     if(__builtin_expect(__builtin_fabsf(x-1.0f)<0x1p-10f, 0)) {
@@ -117,7 +116,7 @@ float cr_logf(float x){
     tz.f = r;
     if(__builtin_expect(!(tz.u&((1u<<28)-1u)), 0) ){
       double dr = (el - r) + f;
-      r += dr*32.0;
+      r += dr*64.0;
       ub = r;
     }
   }
