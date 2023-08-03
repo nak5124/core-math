a64936608cefb731fe01b0bec3b96dc3b7143cf1
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2023-08-03 15:49:23 +0200

    work in progress

--- src/binary64/exp2m1/exp2m1.c
+++ src/binary64/exp2m1/exp2m1.c
@@ -1,4 +1,4 @@
-/* Correctly-rounded expm1 function for binary64 value.
+/* Correctly-rounded exp2m1 function for binary64 value.
 
 Copyright (c) 2022-2023 Paul Zimmermann, Tom Hubrecht and Claude-Pierre Jeannerod
 
@@ -39,6 +39,55 @@ static inline void a_mul(double *hi, double *lo, double a, double b) {
 /****************** end of code copied from pow.[ch] *************************/
 
 typedef union {double f; uint64_t u;} b64u64_u;
+
+/* |x| <= 0.125, put in h + l a double-double approximation of exp2m1(x),
+   and return the maximal corresponding absolute error.
+   We also have |x| > 0x1.0527dbd87e24dp-51. */
+static double
+exp2m1_fast_tiny (double *h, double *l, double x)
+{
+  /* The maximal value of |P_fast_tiny[4]*x^4/expm1(x)| over [-0.125,0.125]
+     is less than 2^-13.495, thus we can compute the coefficients of degree
+     4 or higher using double precision only. */
+  double x2 = x * x, x4 = x2 * x2;
+  double c10 = __builtin_fma (P[11], x, P[10]);
+  double c8 = __builtin_fma (P[9], x, P[8]);
+  double c6 = __builtin_fma (P[7], x, P[6]);
+  double c4 = __builtin_fma (P[5], x, P[4]);
+  c8 = __builtin_fma (c10, x2, c8);
+  c4 = __builtin_fma (c6, x2, c4);
+  c4 = __builtin_fma (c8, x4, c4);
+  double t;
+  // multiply c4 by x and add P[3]
+  a_mul (h, l, c4, x);
+  fast_two_sum (h, &t, P[3], *h);
+  *l += t;
+  // multiply (h,l) by x and add P[2]
+  s_mul (h, l, x, *h, *l);
+  fast_two_sum (h, &t, P[2], *h);
+  *l += t;
+  // multiply (h,l) by x and add P[1]
+  s_mul (h, l, x, *h, *l);
+  fast_two_sum (h, &t, P[1], *h);
+  *l += t;
+  // multiply (h,l) by x
+  s_mul (h, l, x, *h, *l);
+  return 0x1.d4p-65 * *h; // 2^-64.13 < 0x1.d4p-65
+}
+
+/* Given -54 < x < -0x1.0527dbd87e24dp-51 or
+   0x1.0527dbd87e24dp-51 < x < 1024, put in h + l a
+   double-double approximation of exp2m1(x), and return the maximal
+   corresponding absolute error.
+   The input tiny is true iff |x| <= 0.125. */
+static double
+exp2m1_fast (double *h, double *l, double x, int tiny)
+{
+  if (tiny) // |x| <= 0.125
+    return exp2m1_fast_tiny (h, l, x);
+
+  return -2;
+}
 
 double
 cr_exp2m1 (double x)
@@ -106,7 +155,15 @@ cr_exp2m1 (double x)
     }
   }
 
-  /* now -54 < x -0x3cc0527dbd87e24d or 0x3cc0527dbd87e24d < x < 1024 */
+  /* now -54 < x < -0x1.0527dbd87e24dp-51
+     or 0x1.0527dbd87e24dp-51 < x < 1024 */
+
+  double err, h, l;
+  err = exp2m1_fast (&h, &l, x, ax <= 0x3fc0000000000000lu);
+  double left = h + (l - err);
+  double right = h + (l + err);
+  if (left == right)
+    return left;
 
   return -2;
 }
