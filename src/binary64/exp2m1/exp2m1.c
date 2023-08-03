47ea0bb434a3d4c85cdb51ed15c9d89a5116ad8b
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2023-08-03 17:49:34 +0200

    started accurate path for |x| < 0.125

--- src/binary64/exp2m1/exp2m1.c
+++ src/binary64/exp2m1/exp2m1.c
@@ -157,58 +157,95 @@ static const double Q[] = {
 static double
 exp2m1_accurate_tiny (double x)
 {
-  double h, l, t;
-  double x2 = x * x, x4 = x2 * x2;
-  double c13 = __builtin_fma (Q[20], x, Q[19]);
-  double c11 = __builtin_fma (Q[18], x, Q[17]);
-
-#define EXCEPTIONS 0
+#define EXCEPTIONS 28
   static const double exceptions[EXCEPTIONS][3] = {
+    {-0x1.3b6203ff2cbe6p-29, -0x1.b536a7dace8cap-30, 0x1.07bb47158694fp-137},
+    {-0x1.66ff2474821a7p-44, -0x1.f1accede78f51p-45, -0x1.fffffffffffffp-99},
+    {-0x1.0f1c08e43f217p-20, -0x1.77d66368613b4p-21, 0x1.0d3de71c269fcp-130},
+    {-0x1.ba24ff5dea796p-34, -0x1.3278a26ed0162p-34, 0x1.f837135341102p-142},
+    {-0x1.41e4bec9bc547p-19, -0x1.be3d238468f35p-20, -0x1.fffffffffffffp-74},
+    {-0x1.bfb3efcdf2bc4p-10, -0x1.3623eff91de91p-10, 0x1.cd506bd8d0439p-117},
+    {-0x1.9b28778a9a8a3p-4, -0x1.134dec3a6324bp-4, -0x1.1acaaac7e1ff7p-110},
+    {-0x1.922076a30742p-24, -0x1.16bba98a001c1p-24, 0x1.d90eddeada3e4p-133},
+    {-0x1.e07a2fbc7fd84p-30, -0x1.4d0a9e634dc35p-30, -0x1.fffffffffffffp-84},
+    {-0x1.8288f6bb77d79p-23, -0x1.0becf6ad924bfp-23, -0x1.fffffffffffffp-77},
+    {-0x1.d091d774b141ep-35, -0x1.4203e2685b069p-35, -0x1.a166cdf05ff79p-143},
+    {-0x1.f6ec73d3948c3p-4, -0x1.4e2d8b0cead45p-4, -0x1.40f3d5244acffp-109},
+    {-0x1.6f94484e5e1fdp-5, -0x1.f5baee010ccc6p-6, -0x1.3298bcde4f9a8p-115},
+    {-0x1.3918e8608bd5bp-8, -0x1.b153bf52832f9p-9, -0x1.fffffffffffffp-63},
+    {-0x1.a6d6a49f2187fp-50, -0x1.2516dafdf17adp-50, -0x1.85a87dc0b88p-157},
+    {-0x1.daaead688f65fp-45, -0x1.4906541fb8d5cp-45, -0x1.31de54e6dc4ep-151},
+    {-0x1.bfc1e9b0f73aep-35, -0x1.365ca0d933491p-35, -0x1.fffffffffffffp-89},
+    {-0x1.98aae9950914ep-19, -0x1.1b443a4805f8fp-19, 0x1.fffffffffffffp-73},
+    {-0x1.0d296993d1368p-20, -0x1.752326c780f68p-21, 0x1.1cade8dbbd5d8p-130},
+    {-0x1.7b388eb924102p-18, -0x1.06dafba21afffp-18, 0x1.fffffffffffffp-72},
+    {-0x1.3b6786a5a9a69p-17, -0x1.b53dee1a96ca4p-18, -0x1.282e0781f97f5p-124},
+    {-0x1.8474969f5eb14p-9, -0x1.0cfafc07a957bp-9, -0x1.fffffffffffffp-63},
+    {-0x1.a1f242e670d73p-42, -0x1.21b2c54479c4dp-42, 0x1.48a5d173822e8p-150},
+    {-0x1.3c7971b0ee205p-14, -0x1.b6b716c4bb87cp-15, 0x1.d7150e84d973dp-121},
+    {-0x1.0867d8153350dp-9, -0x1.6e49b44e387f5p-10, -0x1.fffffffffffffp-64},
+    {-0x1.0c3ebbd1a501fp-11, -0x1.73ccf8ee62819p-12, 0x1.3a3965a926c6dp-120},
+    {-0x1.0ee8225c19555p-23, -0x1.778e77e8b1e13p-24, -0x1.3da14df43e675p-129},
+    {-0x1.3dbf41403c0b2p-15, -0x1.b87c37192e4f7p-16, 0x1.098861b427b13p-123},
   };
   for (int i = 0; i < EXCEPTIONS; i++)
     if (x == exceptions[i][0])
       return exceptions[i][1] + exceptions[i][2];
 #undef EXCEPTIONS
 
-  c13 = __builtin_fma (c15, x2, c13);
-  // add Q[14]*x+c11*x2+c13*x4 to Q[13]
-  fast_two_sum (&h, &l, Q[13], Q[14] * x + c11 * x2 + c13 * x4);
-  // multiply h+l by x and add Q[12]
+  double h, l, t;
+  double x2 = x * x, x4 = x2 * x2;
+  double c13 = __builtin_fma (Q[20], x, Q[19]); // degree 13
+  double c11 = __builtin_fma (Q[18], x, Q[17]); // degree 11
+  c13 = __builtin_fma (Q[21], x2, c13);         // degree 13
+  // add Q[16]*x+c11*x2+c13*x4 to Q[15] (degree 9)
+  fast_two_sum (&h, &l, Q[15], Q[16] * x + c11 * x2 + c13 * x4);
+  // multiply h+l by x and add Q[14] (degree 8)
+  s_mul (&h, &l, x, h, l);
+  fast_two_sum (&h, &t, Q[14], h);
+  l += t;
+  // multiply h+l by x and add Q[12]+Q[13] (degree 7)
   s_mul (&h, &l, x, h, l);
   fast_two_sum (&h, &t, Q[12], h);
-  l += t;
-  // multiply h+l by x and add Q[10]+Q[11]
+  l += t + Q[13];
+  // multiply h+l by x and add Q[10]+Q[11] (degree 6)
   s_mul (&h, &l, x, h, l);
   fast_two_sum (&h, &t, Q[10], h);
   l += t + Q[11];
-  // multiply h+l by x and add Q[8]+Q[9]
+  // multiply h+l by x and add Q[8]+Q[9] (degree 5)
   s_mul (&h, &l, x, h, l);
   fast_two_sum (&h, &t, Q[8], h);
   l += t + Q[9];
-  // multiply h+l by x and add Q[6]+Q[7]
+  // multiply h+l by x and add Q[6]+Q[7] (degree 4)
   s_mul (&h, &l, x, h, l);
   fast_two_sum (&h, &t, Q[6], h);
   l += t + Q[7];
-  // multiply h+l by x and add Q[4]+Q[5]
+  // multiply h+l by x and add Q[4]+Q[5] (degree 3)
   s_mul (&h, &l, x, h, l);
   fast_two_sum (&h, &t, Q[4], h);
   l += t + Q[5];
-  // multiply h+l by x and add Q[2]+Q[3]
+  // multiply h+l by x and add Q[2]+Q[3] (degree 2)
   s_mul (&h, &l, x, h, l);
   fast_two_sum (&h, &t, Q[2], h);
   l += t + Q[3];
-  // multiply h+l by x and add Q[1]
+  // multiply h+l by x and add Q[0]+Q[1] (degree 2)
   s_mul (&h, &l, x, h, l);
-  fast_two_sum (&h, &t, Q[1], h);
-  l += t;
+  fast_two_sum (&h, &t, Q[0], h);
+  l += t + Q[1];
   // multiply h+l by x
   s_mul (&h, &l, x, h, l);
-  // multiply h+l by x
-  s_mul (&h, &l, x, h, l);
-  // add Q[0]*x = x
-  fast_two_sum (&h, &t, x, h);
-  l += t;
   return h + l;
+}
+
+static double exp2m1_accurate (double x)
+{
+  b64u64_u t = {.f = x};
+  uint64_t ux = t.u, ax = ux & 0x7ffffffffffffffflu;
+
+  if (ax <= 0x3fc0000000000000lu) // |x| <= 0.125
+    return exp2m1_accurate_tiny (x);
+
+  return -2;
 }
 
 double
@@ -265,6 +302,19 @@ cr_exp2m1 (double x)
     }
     else // 0x1.71547652b82fep-968 < |x| <= 0x1.0527dbd87e24dp-51
     {
+#define EXCEPTIONS 6
+      static const double exceptions[EXCEPTIONS][3] = {
+        {-0x1.d8bedc057858cp-65, -0x1.47aea7608c02bp-65, -0x1.ef8a5d5p-172},
+        {-0x1.ec44ae4bc644p-74, -0x1.5536e12eb7335p-74, -0x1.fb8acp-181},
+        {-0x1.f1bc3ef3e6f36p-65, -0x1.5900fbf46981dp-65, -0x1.fffffffffffffp-119},
+        {-0x1.a16826a8e825dp-56, -0x1.21530a306cc85p-56, -0x1.38ac4a67cep-161},
+        {-0x1.8c525b64ed08ep-59, -0x1.12b592f889516p-59, -0x1.7b71d1eep-169},
+        {-0x1.bacdbd3005cd7p-60, -0x1.32ed98e196cf5p-60, -0x1.9b32a24b8p-166},
+      };
+      for (int i = 0; i < EXCEPTIONS; i++)
+        if (x == exceptions[i][0])
+          return exceptions[i][1] + exceptions[i][2];
+#undef EXCEPTIONS
       /* the 2nd term of the Taylor expansion of 2^x-1 at x=0 is
          log(2)^2/2*x^2 */
       static const double C2 = 0x1.ebfbdff82c58fp-3; // log(2)^2/2
@@ -273,6 +323,7 @@ cr_exp2m1 (double x)
       /* we add C2*x^2 last, so that in case there is a cancellation in
          LN2L*x+l, it will contribute more bits */
       l += C2 * x * x;
+      if (x == -0x1.f1bc3ef3e6f36p-65) printf ("h=%la l=%la\n", h, l);
       return h + l;
     }
   }
@@ -287,5 +338,5 @@ cr_exp2m1 (double x)
   if (left == right)
     return left;
 
-  return -2;
+  return exp2m1_accurate (x);
 }
