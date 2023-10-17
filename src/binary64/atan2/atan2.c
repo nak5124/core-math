18e733e8d113c9377a5abd2458bffdc1e67598b6
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2023-10-17 20:28:16 +0200

    work in progress

--- src/binary64/atan2/atan2.c
+++ src/binary64/atan2/atan2.c
@@ -35,6 +35,7 @@ SOFTWARE.
 #pragma STDC FENV_ACCESS ON
 
 typedef union { double f; uint64_t u; } d64u64;
+#include "tint.h"
 
 // return non-zero if u (with sign bit cleared) encodes Inf or NaN
 static inline int inf_or_nan (uint64_t u)
@@ -56,9 +57,64 @@ static inline int is_inf (uint64_t u)
   return (u & MASK) == (0x7fful << 52);
 }
 
+// PI_H+PI_L approximates pi with error bounded by 2^-108.041
+#define PI_H 0x1.921fb54442d18p+1
+#define	PI_L 0x1.1a62633145c07p-53
 // PI_OVER2_H+PI_OVER2_L approximates pi/2 with error bounded by 2^-109.041
 #define PI_OVER2_H 0x1.921fb54442d18p+0
 #define	PI_OVER2_L 0x1.1a62633145c07p-54
+// PI_OVER4_H+PI_OVER4_L approximates pi/4 with error bounded by 2^-110.041
+#define PI_OVER4_H 0x1.921fb54442d18p-1
+#define	PI_OVER4_L 0x1.1a62633145c07p-55
+
+/* The following is a degree-15 polynomial with odd coefficients
+   approximating atan(x) on [0,2^-11.2] with maximal relative error
+   2^-192.031 (cf atan2_small.sollya). */
+static const tint_t Psmall[] = {
+  // degree 1: 1
+  {.h=0x8000000000000000, .m=0x0, .l=0x0, .ex=1, .sgn=0},
+  // degree 3: -0x1.5555555555555555555555555555555555555555569de77ep-2
+  {.h=0xaaaaaaaaaaaaaaaa, .m=0xaaaaaaaaaaaaaaaa, .l=0xaaaaaaaaab4ef3bf, .ex=-1, .sgn=1},
+  // degree 5: 0x1.9999999999999999999999999999999999be8f1d48d2355cp-3
+  {.h=0xcccccccccccccccc, .m=0xcccccccccccccccc, .l=0xccdf478ea4691aae, .ex=-2, .sgn=0},
+  // degree 7: -0x1.2492492492492492492492492492c75ep-3
+  {.h=0x9249249249249249, .m=0x24924924924963af, .l=0x0, .ex=-2, .sgn=1},
+  // degree 9: 0x1.c71c71c71c71c71c71c71dbb1af83beap-4
+  {.h=0xe38e38e38e38e38e, .m=0x38e38edd8d7c1df5, .l=0x0, .ex=-3, .sgn=0},
+  // degree 11: -0x1.745d1745d1745d18p-4
+  {.h=0xba2e8ba2e8ba2e8c, .m=0x0, .l=0x0, .ex=-3, .sgn=1},
+  // degree 13: 0x1.3b13b13b13d919bap-4
+  {.h=0x9d89d89d89ec8cdd, .m=0x0, .l=0x0, .ex=-3, .sgn=0},
+  // degree 15: -0x1.1111103a0ee2178ep-4
+  {.h=0x8888881d07710bc7, .m=0x0, .l=0x0, .ex=-3, .sgn=1},
+};
+
+static double
+atan2_accurate_small (double y, double x)
+{
+  tint_t z[1], z2[1], p[1];
+  div_tint (z, y, x);
+  mul_tint (z2, z, z);
+  cp_tint (p, Psmall+7); // degree 15
+  for (int i = 6; i >= 0; i--)
+  {
+    mul_tint (p, p, z2);
+    add_tint (p, p, Psmall+i);
+  }
+  // multiply by z
+  mul_tint (p, p, z);
+  return tint_tod (p);
+}
+
+// accurate path, assumes both y and x are neither NaN, nor +/-Inf, nor +/-0
+static double
+atan2_accurate (double y, double x)
+{
+  double z = y / x;
+  if (__builtin_fabs (z) <= 0x1.bdb8cdadbe12p-12) // |z| < 2^-11.2
+    return atan2_accurate_small (y, x);
+  return 0;
+}
 
 // atan(y/x)
 double cr_atan2 (double y, double x)
@@ -75,18 +131,16 @@ double cr_atan2 (double y, double x)
     {
       // atan2 (+/-Inf,-Inf) = +/-3pi/4
       if (x < 0)
-        return (y > 0) ? 3 * PI_OVER2_H / 2 + 3 * PI_OVER2_L / 2
-          : -3 * PI_OVER2_H / 2 - 3 * PI_OVER2_L / 2;
+        return (y > 0) ? 3 * PI_OVER4_H + 3 * PI_OVER4_L
+          : -3 * PI_OVER4_H - 3 * PI_OVER4_L;
       // atan2 (+/-Inf,+Inf) = +/-pi/4
-      return (y > 0) ? PI_OVER2_H / 2 + PI_OVER2_L / 2
-        : -PI_OVER2_H / 2 - PI_OVER2_L / 2;
+      return (y > 0) ? PI_OVER4_H + PI_OVER4_L : -PI_OVER4_H - PI_OVER4_L;
     }
     // now only one of y and x is +/-Inf
     if (is_inf (ax))
     {
       if (x < 0)
-        return (uy.u >> 63) ? -2 * PI_OVER2_H - 2 * PI_OVER2_L
-          : +2 * PI_OVER2_H + 2 * PI_OVER2_L;
+        return (uy.u >> 63) ? -PI_H - PI_L : PI_H + PI_L;
       // atan2(+/-0,x) = +/-0 for x > 0
       // atan2(+/-y,+Inf) = +/-0 for finite y>0
       return __builtin_copysign (0, y);
@@ -103,8 +157,7 @@ double cr_atan2 (double y, double x)
       if (ux.u == 0) // atan2(+/-0, +0) = +/-0
         return y;
       // atan2(+/-0, +0) = +/-pi
-      return (uy.u == 0) ? 2 * PI_OVER2_H + 2 * PI_OVER2_L
-        : -2 * PI_OVER2_H - 2 * PI_OVER2_L;
+      return (uy.u == 0) ? PI_H + PI_L : -PI_H - PI_L;
     }
     // only one of y and x is zero
     if (y == 0)
@@ -112,14 +165,17 @@ double cr_atan2 (double y, double x)
       // atan2(+/-0,x) = +/-0 for x>0
       if (x > 0) return y;
       // atan2(+/-0,x) = +/-pi for x<0
-      return (uy.u == 0) ? 2 * PI_OVER2_H + 2 * PI_OVER2_L
-        : -2 * PI_OVER2_H - 2 * PI_OVER2_L;
+      return (uy.u == 0) ? PI_H + PI_L : -PI_H - PI_L;
     }
     // now only x is zero
     // atan2(y,+/-0) = -pi/2 for y<0
     // atan2(y,+/-0) = +pi/2 for y>0
     return (y > 0) ? PI_OVER2_H + PI_OVER2_L : -PI_OVER2_H - PI_OVER2_L;
   }
+
+  // now both y and x are neither NaN, nor +/-Inf, nor +/-0
+
+  return atan2_accurate (y, x);
 
   return 0.0;
 }
---
+++ src/binary64/atan2/tint.h
@@ -0,0 +1,320 @@
+/* Fast 192-bit arithmetic routines.
+
+Copyright (c) 2023 Paul Zimmermann
+
+This file is part of the CORE-MATH project
+(https://core-math.gitlabpages.inria.fr/).
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+typedef unsigned __int128 u128;
+
+// the following represent (-1)^sgn*(h/2^64+m/2^128+l/2^192)*2^ex
+// we have either h=m=l=0 to represent +0 or -0
+// or the most significant bit of h is 1
+typedef union {
+  struct {
+    uint64_t h, m, l, _unused;
+    int64_t ex;
+    uint64_t sgn;
+  };
+  struct {
+    u128 hh, ll;
+    int64_t _ex;
+    uint64_t _sgn;
+  };
+} tint_t;
+
+// ZERO is a tint_t representation of 0
+static const tint_t ZERO = {.h = 0, .m = 0, .l = 0, .ex = -1076, .sgn = 0};
+
+// ONE is a tint_t representation of 1
+static const tint_t ONE = {
+    .h = 0x8000000000000000, .m = 0, .l = 0, .ex = 1, .sgn = 0};
+
+// Print a tint_t value for debugging purposes
+static inline void print_tint (const tint_t *a) {
+  printf("{.h=0x%lx, .m=0x%lx, .l=0x%lx, .ex=%ld, .sgn=0x%lx}\n",
+         a->h, a->m, a->l, a->ex, a->sgn);
+}
+// Copy a tint_t value
+static inline void cp_tint(tint_t *r, const tint_t *a) {
+  r->hh = a->hh;
+  r->ll = a->ll;
+  r->_ex = a->_ex;
+  r->_sgn = a->_sgn;
+}
+
+// Multiply two tint_t numbers, with error < 10 ulps
+// Overlap between r and a or b is allowed
+static inline void
+mul_tint (tint_t *r, const tint_t *a, const tint_t *b)
+{
+  u128 ah = a->h, am = a->m, al = a->l;
+  u128 bh = b->h, bm = b->m, bl = b->l;
+  // printf ("enter mul_tint: ah=%lx bh=%lx\n", (uint64_t) ah, (uint64_t) bh);
+  u128 rh = ah * bh, rm1 = ah * bm, rm2 = am * bh;
+  u128 rl1 = ah * bl, rl2 = am * bm, rl3 = al * bh;
+  uint64_t h, l, cm;
+  r->h = rh >> 64;
+  r->m = rh; // cast to low 64 bits 
+  // accumulate rm1
+  r->l = rm1; // cast to low 64 bits
+  h = rm1 >> 64;
+  r->m += h;
+  rh += r->m < h;
+  // accumulate rm2
+  l = rm2; // cast to low 64 bits
+  r->l += l;
+  cm = r->l < l;
+  h = rm2 >> 64;
+  r->m += h;
+  rh += r->m < h;
+  // accumulate rl1+rl2+rl3
+  rl1 = (rl1 >> 64) + (rl2 >> 64) + (rl3 >> 64);
+  l = rl1; // cast to low 64 bits
+  r->l += l;
+  cm += r->l < l;
+  // accumulate cm
+  r->m += cm;
+  r->h += r->m < cm;
+
+  r->ex = a->ex + b->ex;
+  r->sgn = a->sgn ^ b->sgn;
+
+  /* Note: if one of the operands was zero, then r->h = r->m = r->l = 0,
+     and the normalization keeps r=0. */
+  if (!(r->h >> 63)) // normalize
+  {
+    r->h = (r->h << 1) | (r->m >> 63);
+    r->m = (r->m << 1) | (r->l >> 63);
+    r->l = r->l << 1;
+    r->ex --;
+  }
+
+  /* We ignored the following terms, denoting B=2^64, related to r->l:
+     am*bl + al*bm <= 2*(B-1)^2/B^2 = 2 - 4/B + 2/B^2
+     al*bl <= (B-1)^2/B^3 = 1/B - 2/B^2 + 1/B^3
+     And we truncated rl1+rl2+rl3:
+     low(rl1) + low(rl2) + low(rl3) <= 3*(B-1)/B = 3-3/B
+     This sums up to: 5 - 6/B + 1/B^3 < 5
+     thus the rounding error is bounded by 5 ulps,
+     and after normalization by 10 ulps. */
+}
+
+// Return non-zero if a = 0
+static inline int
+tint_zero_p (const tint_t *a)
+{
+  return a->h == 0;
+}
+
+static inline int cmp(int64_t a, int64_t b) { return (a > b) - (a < b); }
+static inline char cmpu64(uint64_t a, uint64_t b) { return (a > b) - (a < b); }
+static inline char cmpu128(u128 a, u128 b) { return (a > b) - (a < b); }
+
+// Compare the absolute values of a and b
+// Return -1 if |a| < |b|
+// Return  0 if |a| = |b|
+// Return +1 if |a| > |b|
+static inline int
+cmp_tint_abs (const tint_t *a, const tint_t *b) {
+  if (tint_zero_p (a))
+    return tint_zero_p (b) ? 0 : -1;
+  if (tint_zero_p (b))
+    return +1;
+  int c = cmp (a->ex, b->ex);
+  if (c)
+    return c;
+  // now a->ex = b->ex
+  c = cmpu128 (a->hh, b->hh);
+  if (c)
+    return c;
+  return cmpu128 (a->ll, b->ll);
+}
+
+// Add two tint_t values
+static inline void
+add_tint (tint_t *r, const tint_t *a, const tint_t *b)
+{
+  switch (cmp_tint_abs (a, b))
+  {
+  case 0: // |a| = |b|
+    if (a->sgn ^ b->sgn) {
+      cp_tint (r, &ZERO);
+      return;
+    }
+    cp_tint (r, a);
+    r->ex++;
+    return;
+
+  case -1: // |a| < |b|
+    {
+      // swap operands
+      const tint_t *tmp = a; a = b; b = tmp;
+      break; // fall through the case |a| > |b|
+    }
+  }
+
+  // From now on, |a| > |b| thus a->ex >= b->ex
+  uint64_t k = a->ex - b->ex;
+
+  u128 bh, bl;
+  if (k == 0)
+  {
+    bh = b->hh;
+    bl = b->ll;
+  }
+  else if (k < 128)
+  {
+    bh = b->hh >> k;
+    bl = (b->hh << (128 - k)) | (b->ll >> k);
+  }
+  else if (k < 256)
+  {
+    bh = 0;
+    bl = (k == 128) ? b->hh : b->hh >> (k - 128);
+  }
+  else
+    bh = bl = 0;
+
+  u128 cy, ch, rh, rl;
+  if (a->sgn ^ b->sgn) { // opposite signs, it's a subtraction
+    rl = a->ll - bl;
+    rh = a->hh - bh - (rl > a->ll);
+    uint64_t rhh = rh >> 64, rlh = rl >> 64;
+    uint64_t ex =
+      rhh ? __builtin_clzl (rhh)
+      : 64 + (rh ? __builtin_clzl(rh)
+              : 64 + (rlh ? __builtin_clzl(rlh)
+                      : 64 + __builtin_clzl(rl)));
+    r->ex = a->ex - ex;
+    if (ex == 0) { // nothing to do
+    }
+    else if (ex < 128) {
+      r->hh = (rh << ex) || (rl >> (128 - ex));
+      r->ll = rl << ex;
+    }
+    else { // necessarily ex < 192 since |a| > |b|
+      ex -= 128;
+      r->hh = (ex == 128) ? rl : rl << ex;
+      r->l = 0;
+    }
+  }
+  else { // same signs, it's an addition
+    rl = a->ll + bl;
+    ch = rl < a->ll; // carry at rh
+    rh = a->hh + bh;
+    cy = rh < a->hh; // final carry
+    rh += ch;
+    cy += rh < ch;
+    if (cy) { // can be at most 1
+      r->ex = a->ex + 1;
+      r->hh = (cy << 127) | (rh >> 1);
+      r->ll = (rh << 127) | (rl >> 1);
+    }
+    else {
+      r->ex = a->ex;
+      r->hh = rh;
+      r->ll = rl;
+    }
+  }
+  r->sgn = a->sgn;
+}
+
+// a <- x, assuming x is not NaN, Inf or 0
+static inline void tint_fromd (tint_t *a, double x)
+{
+  d64u64 u = {.f = x};
+  a->sgn = u.u >> 63;
+  uint64_t ax = u.u & 0x7ffffffffffffffful;
+  int64_t e = ax >> 52;
+  if (__builtin_expect (e, 1)) { // normal
+    // 1 has e=0x3ff
+    a->ex = e - 0x3fe;
+    a->h = (1ul << 63) | (ax << 11);
+  }
+  else { // subnormal
+    // 2^-1074 has ax=1
+    e = __builtin_clzl (ax);
+    a->ex = -0x3f2 - e;
+    a->h = ax << e;
+  }
+  a->m = a->l = 0;
+}
+
+static inline double tint_tod (tint_t *a)
+{
+  if (a->ex >= 1025) // overflow: |a| >= 2^1024
+    return a->sgn ? -0x1p1023 - 0x1p1023 : 0x1p1023 + 0x1p1023;
+  if (a->ex <= -1074) // underflow: |a| < 2^-1074
+  {
+    if (a->ex < -1074) // |a| < 2^-1075
+      return (a->sgn ? -0x1p-1074 : 0x1p-1074) * 0.5;
+    // 2^-1075 <= |a| < 2^-1074
+    int mid = a->h == (1ul << 63) && a->m == 0 && a->l == 0;
+    // if mid, |a| = 2^-1075
+    return (a->sgn ? -0x1p-1074 : 0x1p-1074) * (mid ? 0.5 : 0.75);
+  }
+  double r[4];
+  r[3] = a->h >> 11; // 53 bits from a->h
+  r[2] = (a->h << 53) | (a->m >> 22); // 11 bits from a->h, 42 from a->m
+  r[1] = (a->m << 42) | (a->l >> 33); // 22 bits from a->m, 31 from a->l
+  r[0] = a->l << 31; // 33 bits from a->l
+  r[1] = __builtin_fma (r[0], 0x1p-53, r[1]);
+  r[2] = __builtin_fma (r[1], 0x1p-53, r[2]);
+  r[3] = __builtin_fma (r[2], 0x1p-53, r[3]);
+  r[3] *= 0x1p-53;
+  d64u64 u = {.f = 1.0};
+  u.u += (a->ex - 0x3ff) << 52;
+  return r[3] * u.f;
+}
+
+// put in r a 106-bit approximation of 1/a, assuming a is not zero
+// assume A = tint_fromd (a)
+static inline void inv_tint (tint_t *r, const tint_t *A, double a)
+{
+  tint_t q[1];
+  tint_fromd (r, 1.0 / a); // accurate to about 53 bits
+  // we use Newton's iteration: r -> r + r*(1-a*r)
+  mul_tint (q, A, r);      // a*r
+  q->sgn = 1 - q->sgn;     // -a*r
+  add_tint (q, &ONE, q);   // 1-a*r
+  mul_tint (q, r, q);      // r*(1-a*r)
+  add_tint (r, r, q);
+}
+
+// put in r an approximation of b/a, assuming a is not zero
+static inline void div_tint (tint_t *r, double b, double a)
+{
+  tint_t A[1], B[1], Y[1], Z[1];
+  tint_fromd (A, a);
+  tint_fromd (B, b);
+  inv_tint (Y, A, a); // Y = 1/a to about 106 bits
+  mul_tint (r, Y, B); // r = b/a to about 106 bits
+  // we use Karp-Markstein's trick: r' = r + y*(b-a*r)
+  mul_tint (Z, A, r);  // a*r
+  Z->sgn = 1 - Z->sgn; // -a*r
+  add_tint (Z, B, Z);  // b-a*r
+  mul_tint (Z, Y, Z);  // y*(b-a*r)
+  add_tint (r, r, Z);
+}
+
+
