655f81edc80bf3d24452e2046cf8643735e3b49c
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2023-12-12 12:45:07 +0100

    start work on atan2pi (reusing atan2)

---
+++ src/binary64/atan2pi/function_under_test.h
@@ -0,0 +1,9 @@
+#define cr_function_under_test cr_atan2pi
+#define ref_function_under_test ref_atan2pi
+
+#define EXCHANGE_X_Y
+
+#define WORST_SYMMETRIC_X // test x and -x
+#define WORST_SYMMETRIC_Y // test y and -y
+#define WORST_SWAP        // test x,y and y,x
+
