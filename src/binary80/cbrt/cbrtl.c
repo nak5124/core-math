ff0a682f65724ac0f882da4c4f1c89194ad58f93
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2024-04-04 13:42:26 +0200

    cleanup of the code before review

--- src/binary80/cbrt/cbrtl.c
+++ src/binary80/cbrt/cbrtl.c
@@ -1,4 +1,4 @@
-/* Correctly rounded cbrtl function for binary64 values.
+/* Correctly rounded cbrt function for long-double values.
 
 Copyright (c) 2024 Alexei Sibidanov and Paul Zimmermann
 
@@ -27,15 +27,15 @@ SOFTWARE.
 /* References:
    [1] Note on the Veltkamp/Dekker Algorithms with Directed Roundings,
        Paul Zimmermann, https://inria.hal.science/hal-04480440, February 2024.
-   [2] CR-LIBM A library of correctly rounded elementary functions in double-precision,
-       Catherine Daramy-Loirat, David Defour, Florent de Dinechin, Matthieu Gallet, Nicolas Gast,
-       Christoph Lauter, Jean-Michel Muller, https://ens-lyon.hal.science/ensl-01529804, 2017.
-   [3] Handbook of Floating-Point Arithmetic, Jean-Michel Muller, Nicolas Brunie, Florent de Dinechin,
-       Claude-Pierre Jeannerod, Mioara Joldes, Vincent Lefèvre, Guillaume Melquiond, Nathalie Revol,
-       Serge Torres, 2018.
+   [2] CR-LIBM A library of correctly rounded elementary functions in
+       double-precision, Catherine Daramy-Loirat, David Defour, Florent de
+       Dinechin, Matthieu Gallet, Nicolas Gast, Christoph Lauter, Jean-Michel
+       Muller, https://ens-lyon.hal.science/ensl-01529804, 2017.
+   [3] Handbook of Floating-Point Arithmetic, Jean-Michel Muller, Nicolas
+       Brunie, Florent de Dinechin, Claude-Pierre Jeannerod, Mioara Joldes,
+       Vincent Lefèvre, Guillaume Melquiond, Nathalie Revol, Serge Torres,
+       2018.
  */
-
-#define TRACE -0x9.130930a911309f4p+6808L
 
 #include <stdio.h>
 #include <stdint.h>
@@ -50,25 +50,16 @@ SOFTWARE.
 // anonymous structs, see https://port70.net/~nsz/c/c11/n1570.html#6.7.2.1p19
 typedef union {
   long double f;
-  struct __attribute__((__packed__)) {uint64_t m; uint32_t e:16; uint32_t empty:16;};
+  struct __attribute__((__packed__))
+  {uint64_t m; uint32_t e:16; uint32_t empty:16;};
 } b96u96_u;
 
-typedef union {double f;uint64_t u;} b64u64_u;
 /* s + t <- a + b, assuming |a| >= |b| */
 static inline void
 fast_two_sum (long double *s, long double *t, long double a, long double b)
 {
   *s = a + b;
   long double e = *s - a;
-  *t = b - e;
-}
-
-/* s + t <- a + b, assuming |a| >= |b| */
-static inline void
-fast_two_sum_double (double *s, double *t, double a, double b)
-{
-  *s = a + b;
-  double e = *s - a;
   *t = b - e;
 }
 
@@ -111,13 +102,6 @@ static inline void a_mul_double (double *hi, double *lo, double a, double b) {
   *lo = __builtin_fma (a, b, -*hi);
 }
 
-// Return in hi+lo a 128-bit approximation of (ah + al) * b
-static inline void
-s_mul (long double *hi, long double *lo, long double ah, long double al, long double b) {
-  a_mul (hi, lo, ah, b); // exact
-  *lo += al * b;
-}
-
 // Return in hi+lo a 128-bit approximation of (ah + al) * (bh + bl)
 static inline void
 d_mul (long double *hi, long double *lo, long double ah, long double al,
@@ -138,8 +122,8 @@ static inline void d_mul_double (double *hi, double *lo, double ah, double al,
   *lo = __builtin_fma (ah, bl, t);
 }
 
-/* Return err, and update h,l,e such that (h+l)*2^exp is an an approximation of x^(1/3)
-   with absolute error less than err*2^exp. */
+/* Return err, and update h,l,e such that (h+l)*2^exp is an an approximation
+   of x^(1/3) with absolute error less than err*2^exp. */
 static double
 fast_path (long double *h, long double *l, int *exp, long double x)
 {
@@ -156,14 +140,13 @@ fast_path (long double *h, long double *l, int *exp, long double x)
   // now x = (m/2^63)*2^(e-16383) with 2^63 <= m < 2^64
   v.e = 16383; // reduce v.f in [1,2)
   int i = (e + 63) % 3; // we add 63 since e can be negative
-  // if (x == TRACE) printf ("i=%d\n", i);
   *exp = ((e + 63) / 3) - 5482;
   // cbrt(x) = (-1)^s * cbrt(m/2^63) * 2^e * 2^(i/3)
   double xh = v.f;
   double xl = v.f - (long double) xh;
 
-  /* the polynomial c0+c1*x+...+c5*x^5 approximates x^(1/3) on [1,2] with absolute
-     error bounded by 2^-19.473 (cf cbrt.sollya) */
+  /* the polynomial c0+c1*x+...+c5*x^5 approximates x^(1/3) on [1,2] with
+     absolute error bounded by 2^-19.473 (cf cbrt.sollya) */
   static const double c[] = {0x1.e53b7c444f1cep-2, 0x1.ac2d3134803e2p-1,
                              -0x1.ddcd3b46e2071p-2, 0x1.9b95b5c19bd0bp-3,
                              -0x1.97bd99b63f65ep-5, 0x1.592445ed9c63ap-8};
@@ -180,23 +163,24 @@ fast_path (long double *h, long double *l, int *exp, long double x)
      Write a = x, and x0 = a^(1/3) + e0, with |e0| < 2^-19.473.
      Then x0^3 - a = 3*a^(2/3)*e0 + 3*a^(1/3)*e0^2 + e0^3.
      Ignoring rounding errors, we have:
-     h0 = (x0^3-a)/a = 3*a^(-1/3)*e0 + f with |f| = |(3*a^(1/3)*e0^2 + e0^3)/a|.
-     The maximum of (3*a^(1/3)*e0^2 + e0^3)/a is attained at a=1, thus |f| < 2^-37.36.
-     Since |h0| < 2^-17, ulp(h0) <= 2^-70, the difference between |(3*a^(1/3)*e0^2 + e0^3)/a|
-     and 2^-37.36 is more than 4e6 ulp(h0), thus the bound 2^-37.36 clearly includes
-     the rounding errors. */
+     h0 = (x0^3-a)/a = 3*a^(-1/3)*e0 + f with |f|=|(3*a^(1/3)*e0^2 + e0^3)/a|.
+     The maximum of (3*a^(1/3)*e0^2 + e0^3)/a is attained at a=1, thus
+     |f| < 2^-37.36. Since |h0| < 2^-17, ulp(h0) <= 2^-70, the difference
+     between |(3*a^(1/3)*e0^2 + e0^3)/a| and 2^-37.36 is more than 4e6 ulp(h0),
+     thus the bound 2^-37.36 clearly includes the rounding errors. */
 
 #define MINUS_ONE_THIRD -0x1.5555555555555p-2
   double x1 = __builtin_fma (x0 * h0, MINUS_ONE_THIRD, x0);
   /* x0*h0/3 = (a^(1/3) + e0) * (a^(-1/3)*e0 + f/3)
              = e0 + a^(1/3)*f/3 + a^(-1/3)*e0^2 + e0*f/3
-     Ignoring rounding errors, the maximum of a^(1/3)*f/3 + a^(-1/3)*e0^2 + e0*f/3
-     is attained at a=2, thus:
+     Ignoring rounding errors, the maximum of a^(1/3)*f/3 + a^(-1/3)*e0^2
+     + e0*f/3 is attained at a=2, thus:
      x0*h0/3 = e0 - e1 with |e1| < 2^-37.90.
      Thus x1 = a^(1/3) + e1 with |e1| < 2^-37.90.
      Since |x0*h0/3| < 2^-19, ulp(x0*h0/3) <= 2^-72, the difference between
-     |a^(1/3)*f/3 + a^(-1/3)*e0^2 + e0*f/3| and 2^-37.90 is more than 9e7 ulp(x0*h0/3),
-     thus the bound 2^-37.90 clearly includes the rounding errors. */
+     |a^(1/3)*f/3 + a^(-1/3)*e0^2 + e0*f/3| and 2^-37.90 is more than
+     9e7 ulp(x0*h0/3), thus the bound 2^-37.90 clearly includes the
+     rounding errors. */
 
   double th, tl;
   a_mul_double (&th, &tl, x1, x1); // x1^2 = th + tl
@@ -207,24 +191,28 @@ fast_path (long double *h, long double *l, int *exp, long double x)
      (this bound is attained in a=2, for x1 = a^(1/3) + 2^-37.90).
      Since h1 is a correction term, we can compute it in double precision only.
      Ignoring rounding errors again, we have:
-     h1 = (x1^3-a)/a = 3*a^(-1/3)*e1 + f' with |f'| = |(3*a^(1/3)*e1^2 + e1^3)/a|.
-     The maximum of (3*a^(1/3)*e1^2 + e1^3)/a is attained at a=1, thus |f'| < 2^-74.21.
+     h1 = (x1^3-a)/a = 3*a^(-1/3)*e1 + f', |f'| := |(3*a^(1/3)*e1^2 + e1^3)/a|.
+     The maximum of (3*a^(1/3)*e1^2 + e1^3)/a is attained at a=1, thus
+     |f'| < 2^-74.21.
      Now let us analyze rounding errors:
      * a_mul_double is exact, thus there is no rounding error
      * the rounding error in h1 is bounded by ulp(h1).
        Since |h1| < |3*a^(-1/3)*e1 + f'| < 2^-36, ulp(h1) <= 2^-89.
-       This error is multiplied by r with |r| <= 1 thus contributes to at most 2^-89 in h1.
+       This error is multiplied by r with |r| <= 1 thus contributes to
+       at most 2^-89 in h1.
      * the rounding error in h1l is bounded by ulp(h1l).
-       We have |tl| <= ulp(th), where |th| <= x1^2 < 2, thus |tl| <= ulp(th) <= 2^-52.
+       We have |tl| <= ulp(th), where |th| <= x1^2 < 2, thus
+       |tl| <= ulp(th) <= 2^-52.
        Since |x1| < 2, this yields |tl*x1| < 2^-51.
-       Now |xl| <= ulp(xh) <= 2^-52, thus |h1l| < 2^-51+2^-52, and the rounding error on tl
-       is bounded by ulp(2^-51+2^-52) = 2^-103.
-       This error is multiplied by r with |r| <= 1 thus contributes to at most 2^-103 in h1.
-     * |h1 + h1l| < |3*a^(-1/3)*e1 + f' + 2^-51+2^-52| < 2^-36, thus the rounding error on
-       h1 + h1l is bounded by ulp(2^-37) = 2^-89, and is multiplied by r with |r| <= 1
-       thus contributes to at most 2^-89 in h1.
-     * |(h1 + h1l) * r| < 2^-36, thus the rounding error in the product is bounded by
-       ulp(2^-37) = 2^-89.
+       Now |xl| <= ulp(xh) <= 2^-52, thus |h1l| < 2^-51+2^-52, and the
+       rounding error on tl is bounded by ulp(2^-51+2^-52) = 2^-103.
+       This error is multiplied by r with |r| <= 1 thus contributes to at
+       most 2^-103 in h1.
+     * |h1 + h1l| < |3*a^(-1/3)*e1 + f' + 2^-51+2^-52| < 2^-36, thus the
+       rounding error on h1 + h1l is bounded by ulp(2^-37) = 2^-89, and is
+       multiplied by r with |r| <= 1 thus contributes to at most 2^-89 in h1.
+     * |(h1 + h1l) * r| < 2^-36, thus the rounding error in the product is
+       bounded by ulp(2^-37) = 2^-89.
      The contribution of the rounding errors is thus bounded by:
      2^-89 + 2^-103 + 2^-89 + 2^-89 < 2^-87.41. This is much smaller than
      the difference between (3*a^(1/3)*e1^2 + e1^3)/a and the bound 2^-74.21,
@@ -232,25 +220,27 @@ fast_path (long double *h, long double *l, int *exp, long double x)
 
   /* x1*h1/3 = (a^(1/3) + e1) * (a^(-1/3)*e1 + f'/3)
              = e1 + a^(1/3)*f'/3 + a^(-1/3)*e1^2 + e1*f'/3
-     The maximum of a^(1/3)*f'/3 + a^(-1/3)*e1^2 + e1*f'/3 is attained at a=2, thus:
-     x1*h1/3 = e1 - e2 with |e2| < 2^-74.75.
+     The maximum of a^(1/3)*f'/3 + a^(-1/3)*e1^2 + e1*f'/3 is attained at a=2,
+     thus: x1*h1/3 = e1 - e2 with |e2| < 2^-74.75.
      Thus x1 - x1*h1/3 = a^(1/3) + e2 with |e2| < 2^-74.75. */
 
   double corr = (x1 * h1) * MINUS_ONE_THIRD;
   /* Here we have two rounding errors:
-     (a) the rounding error on x1 * h1. Since |x1| < 2 and |h1| < 2^-36, |x1*h1| < 2^-35
-         thus this rounding error is bounded by ulp(2^-36) = 2^-88. This rounding error
-         is multiplied by MINUS_ONE_THIRD thus contributes to at most
+     (a) the rounding error on x1 * h1. Since |x1| < 2 and |h1| < 2^-36,
+         |x1*h1| < 2^-35 thus this rounding error is bounded by
+         ulp(2^-36) = 2^-88. This rounding error is multiplied by
+         MINUS_ONE_THIRD thus contributes to at most
          2^-88 * |MINUS_ONE_THIRD| < 2^-89.58.
-     (b) the rounding error on w * MINUS_ONE_THIRD where w = RND(x1*h1). Since |w| < 2^-35
-         and |MINUS_ONE_THIRD| < 1/2, we have |corr| < 2^-36, thus this rounding error is
-         bounded by ulp(corr) <= 2^-89.
+     (b) the rounding error on w * MINUS_ONE_THIRD where w = RND(x1*h1).
+         Since |w| < 2^-35 and |MINUS_ONE_THIRD| < 1/2, we have |corr| < 2^-36,
+         thus this rounding error is bounded by ulp(corr) <= 2^-89.
      The sum (a) + (b) is thus bounded by 2^-89.58 + 2^-89 < 2^-88.26.
      This yields a total error bound for x1 + corr of:
      2^-74.75 + 2^-88.26 < 2^-74.749.
    */
 
-  /* multiply (x1,corr) by 2^(i/3): sh[i]+sl[i] is a double-double approximation of 2^(i/3) */
+  /* multiply (x1,corr) by 2^(i/3): sh[i]+sl[i] is a double-double
+     approximation of 2^(i/3) */
   static const double sh[] = {1.0, 0x1.428a2f98d728bp+0, 0x1.965fea53d6e3dp+0};
   static const double sl[] = {0.0, -0x1.ddc22548ea41ep-56, -0x1.f53e999952f09p-54};
   d_mul_double (&x1, &corr, x1, corr, sh[i], sl[i]);
@@ -289,13 +279,9 @@ accurate_path (long double h, long double l, int e, long double x)
      (2) then compute x3 = x2 - x2*e2/3
   */
 
-  //  long double x0 = x;
-  // if (x0 == TRACE) printf ("enter accurate_path: h=%La l=%La\n", h, l);
-
   /* Rescale x so that 1 <= x < 8. With x' = 2^(-3e)*x, we have 1 <= x' < 8
      and h+l is an approximation of cbrt(x'), thus 1 <= h+l <= 2. */
   x = __builtin_ldexpl (x, -3 * e);
-  // if (x0 == TRACE) printf ("x rescaled=%La\n", x);
 
   // detect exact cases
   long double t = round22 (h);
@@ -304,38 +290,25 @@ accurate_path (long double h, long double l, int e, long double x)
 
   // normalize h+l
   fast_two_sum (&h, &l, h, l);
-  // if (x0 == TRACE) printf ("after normalize: h=%La l=%La\n", h, l);
 
   long double yh, yl;
   // compute yh+yl = (h+l)^3
   d_mul (&yh, &yl, h, l, h, l);
   d_mul (&yh, &yl, yh, yl, h, l);
-  // if (x0 == TRACE) printf ("yh=%La yl=%La\n", yh, yl);
   // subtract x and normalize
   yh = yh - x;
-  // if (x0 == TRACE) printf ("yh=%La\n", yh);
   yh = yh + yl;
-  // if (x0 == TRACE) printf ("yh=%La\n", yh);
-  /* since we had a 75-bit accurate approximation, |yh| should be bounded by 2^-75
-     thus working with a single long double is enough */
+  /* since we had a 75-bit accurate approximation, |yh| should be bounded
+     by 2^-75 thus working with a single long double is enough */
   yh = yh / x; // approximates ((h+l)^3 - x) / x
-  // if (x0 == TRACE) printf ("yh=%La\n", yh);
 
   // multiply yh by h
   yh = yh * h;
-  // if (x0 == TRACE) printf ("yh=%La\n", yh);
   // divide by -3
 #define MINUS_ONE_THIRD_L -0x1.5555555555555556p-2L
   yh = yh * MINUS_ONE_THIRD_L;
-  // if (x0 == TRACE) printf ("yh=%La\n", yh);
   // add to lower term
   l += yh;
-
-  //  if (x0 == TRACE) printf ("h=%La l=%La e=%d\n", h, l, e);
-
-  /* FIXME: to detect exact cases, round h to nearest to 22 bits of precision,
-     then take the cube, and check it matches x rescaled. We can do that only
-     when l is small enough or near enough from ulp(1). */
 
 #define EXCEPTIONS 10
   static const long double exceptions[EXCEPTIONS][3] = {
@@ -374,19 +347,15 @@ cr_cbrtl (long double x)
 
   long double h, l;
   long double err = fast_path (&h, &l, &e, x);
-  // if (x == TRACE) printf ("h=%La l=%La\n", h, l);
   long double left = h + (l - err);
   long double right = h + (l + err);
   if (left == right)
   {
-    // if (x == TRACE) printf ("fast path succeeded\n");
     // multiply left by 2^e
     b96u96_u v = {.f = left};
     v.e += e;
     return v.f;
   }
-
-  // if (x == TRACE) printf ("fast path failed\n");
 
   // we reuse the initial approximation (h+l)*2^e in the accurate path
   return accurate_path ((long double) h, (long double) l, e, x);
