nak5124/core-math

commit 02b0890175f3c8969998e4c9eef5252789f2b438

avoid spurious inexact exception for log(1) = 0

src/binary64/log/log.c
@@ -670,6 +670,8 @@ cr_log (double x)
   /* normalize v in [1,2) */
   v.u = (0x3fful << 52) | (v.u & 0xfffffffffffff);
   /* now x = m*2^e with 1 <= m < 2 (m = v.f) and -1074 <= e <= 1023 */
+  if (__builtin_expect (e == 0 && v.u == 0x3ff0000000000000ul, 0))
+    return 0; // log(1) = 0, avoids spurious inexact exception
   double h, l;
   cr_log_fast (&h, &l, e, v);
 

commit 7a389731bc24c0be2286c21ba138e5eb168ad9a1

speedup for log
We had a regression in revision 02b0890 (avoids spurious inexact exception
for log(1)), where the reciprocal throughput increased from 15.563 to 23.393.
By comparing first v.u, we get 16.612.

src/binary64/log/log.c
@@ -670,8 +670,8 @@ cr_log (double x)
   /* normalize v in [1,2) */
   v.u = (0x3fful << 52) | (v.u & 0xfffffffffffff);
   /* now x = m*2^e with 1 <= m < 2 (m = v.f) and -1074 <= e <= 1023 */
-  if (__builtin_expect (e == 0 && v.u == 0x3ff0000000000000ul, 0))
-    return 0; // log(1) = 0, avoids spurious inexact exception
+  if (__builtin_expect (v.u == 0x3ff0000000000000ul && e == 0, 0))
+    return 0;
   double h, l;
   cr_log_fast (&h, &l, e, v);
 