5ea9eac2ccea97f88cc3afe03b1340794e1712ec
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2024-08-07 15:45:00 +0200

    [powl] completed review of qint_told()

--- src/binary80/pow/qint.h
+++ src/binary80/pow/qint.h
@@ -403,61 +403,69 @@ long double qint_told(qint64_t* a, uint64_t extralow,
 	POWL_DPRINTF("m = %016lx\n", v.m);
 	POWL_DPRINTF("e = %x (%ld)\n", v.e, a->ex);
 
-	/* d/2^128 holds the signed distance to the rounding boundary */
-	unsigned __int128 d = a->hl; d <<= 64;
-	d += a->lh;
+	/* d/2^128 is the signed distance to the rounding boundary,
+           in terms of ulp(v), with error less than 1/2^-128 */
+	unsigned __int128 d = ((__int128) a->hl << 64) + a->lh;
 
-	/* The relative error is bounded by 2^-234.862
-           (see analysis of cr_powl in powl.c).
+	/* The relative error is bounded by 2^-234.861 (see analysis of
+           cr_powl in powl.c) thus by 2^(64-234.861) = 2^-170.861 ulp(v).
 	   Therefore, if d != 0 and d != -1, then the rounding test must pass
 	   because this implies that the distance to the rounding boundary is
-	   at least 2^(-64-128) > 2^-234.861 (assume a normal result).
+	   at least (1-2^-128)/2^128 ulp > 2^-170.861 ulp for a normal result.
 	   If we are in the denormal range, d has even more relative weight
 	   and this stays true.
 	*/
 	if(__builtin_expect(d != 0 && d+1 != 0, 1)) {*hard = false;}
 	else {
-		d = d << 64; d += a->ll; uint64_t d_extra = extralow;
+          d = (d << 64) + a->ll;
+          uint64_t d_extra = extralow;
 
-	  /* d holds the distance to the rounding boundary, scaled by 2^-128.*/
-		unsigned __int128 eps = a->hh; eps <<= 64;
-		uint64_t eps_extra  = 0;
+	  /* d/2^192 is the signed distance to the rounding boundary,
+             in terms of ulp(v) */
+          unsigned __int128 eps = (__int128) a->hh << 64;
+          uint64_t eps_extra  = 0;
 
-	  /* If we didn't do anymore scaling this would correspond to a relative
-	     error of 2^-128. The relative error is < 2^-234.861
-		   therefore, shift by 234 - 128 right.
-	  */
-		eps_extra = (uint64_t)(eps >> (234 - 128 - 64));
+          /* Not counting the lower limbs, the error bound is
+             2^-234.861*|v| <= 2^-234.861 * hh * ulp(v),
+             where hh is interpreted as a 64-bit integer.
+             To align with d, which is the signed distance in terms of
+             2^-192 ulp(v), we have multiply by 2^192 and divide by ulp(v):
+             2^192 * 2^-234.861 * hh = 2^-42.861 * hh,
+             where hh is interpreted as a 64-bit integer.
+             In terms of eps which is a 128-bit integer this yields:
+             2^-64 * 2^-42.861 * eps < 2^-106 * eps */
+
+          eps_extra = (uint64_t) (eps >> (106 - 64));
 	  // Grab the highest low bits which would be lost
-		eps >>= 234 - 128;
-		eps_extra += 1; // Make sure we over-approximate eps.
+          eps >>= 106;
+          eps_extra += 1; // Make sure we over-approximate eps.
 
-		/* The scaling introduces an error at most 1 lsb of eps_extra. We know that
-		   initially a's MSB is at most at position 65 counting from the highest bit
-		   of the mantissa. Therefore 1 lsb of eps_extra has relative weight
-	     2^(-255 - 64 + 65) = 2^-254.
+          /* The scaling introduces an error at most 1 lsb of eps_extra.
+             We know that initially a's MSB is at most at position 65 counting
+             from the highest bit of the mantissa. Therefore 1 lsb of eps_extra
+             has relative weight 2^(-255 - 64 + 65) = 2^-254.
 
 	     Failure of the rounding test therefore implies that
 	       |z - r| <= (2^-234 + 2^-254)|z|
-		*/
+          */
 
-		/* Implements the usual rounding test d + eps <= 2*eps on 192 bits */
+          /* Implements the usual rounding test d + eps <= 2*eps on 192 bits */
 		uint64_t d_plus_eps_extra = eps_extra + d_extra; 
 		unsigned __int128 d_plus_eps = d + eps;
-		if(d_plus_eps_extra < eps_extra) {
-			d_plus_eps++;
-		}
+		if (d_plus_eps_extra < eps_extra)
+                  d_plus_eps++; // carry in eps_extra + d_extra
 
 		uint64_t eps2_extra = eps_extra << 1;
 		unsigned __int128 eps2 = (eps << 1) | (eps_extra >> 63);
+                // eps2 + eps2_extra/2^64 = 2*(eps + eps_extra/2^64)
 
 		*hard = (d_plus_eps < eps2) ||
 	          (d_plus_eps == eps2 && d_plus_eps_extra < eps2_extra);
 	}
 
-	if(__builtin_expect(a->ex >= 16384, 0)) {
-			return invert ? (-0x1p16383L - 0x1p16383L) : (0x1p16383L + 0x1p16383L);
-	}
+	if(__builtin_expect(a->ex >= 16384, 0)) // overflow case
+          return invert ? (-0x1p16383L - 0x1p16383L)
+            : (0x1p16383L + 0x1p16383L);
 	return v.f;
 }
 
