78723e54fde160cdebe6c6b870ac69a59db0875c
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2024-10-18 10:50:41 +0200

    added sincos (work in progress)

--- src/binary64/sincos/sincos.c
+++ src/binary64/sincos/sincos.c
@@ -1,6 +1,6 @@
-/* Correctly-rounded sine function for binary64 value.
+/* Correctly-rounded sincos for binary64 value.
 
-Copyright (c) 2022-2023 Paul Zimmermann and Tom Hubrecht
+Copyright (c) 2022-2024 Paul Zimmermann and Tom Hubrecht
 
 This file is part of the CORE-MATH project
 (https://core-math.gitlabpages.inria.fr/).
@@ -89,14 +89,6 @@ dint_zero_p (const dint64_t *a)
 {
   return a->hi == 0;
 }
-
-#if 0
-// Prints a dint64_t value for debugging purposes
-static inline void print_dint(const dint64_t *a) {
-  printf("{.hi=0x%"PRIx64", .lo=0x%"PRIx64", .ex=%"PRId64", .sgn=0x%"PRIx64"}\n", a->hi, a->lo, a->ex,
-         a->sgn);
-}
-#endif
 
 static inline int cmp(int64_t a, int64_t b) { return (a > b) - (a < b); }
 
@@ -1724,9 +1716,11 @@ reduce_fast (double *h, double *l, double x, double *err1)
   return i;
 }
 
-/* return the maximal absolute error */
+/* Put in h+l an approximation of sin(x), and in hc+lc an approximation of cos(x),
+   and return the maximal absolute error (for both).
+   Adapted from sin_fast in sin.c */
 static double
-sin_fast (double *h, double *l, double x)
+sincos_fast (double *h, double *l, double *hc, double *lc, double x)
 {
   int neg = x < 0, is_sin = 1;
   double absx = neg ? -x : x;
@@ -1802,38 +1796,47 @@ sin_fast (double *h, double *l, double x)
   /* the relative error of evalPCfast() is less than 2^-69.96 from
      routine evalPCfast(rel=true) in sin.sage:
      | ch + cl - cos2pi(h+l) | < 2^-69.96 * |ch + cl| */
-  double err;
+  double Sh, Sl, Ch, Cl;
   static const double sgn[2] = {1.0, -1.0};
   if (is_sin)
     {
-      s_mul (&sh, &sl, sgn[neg] * SC[i][2], sh, sl);
-      s_mul (&ch, &cl, sgn[neg] * SC[i][1], ch, cl);
-      fast_two_sum (h, l, ch, sh);
-      *l += sl + cl;
+      /* sin(x) ~ SC[i][2] * (sh + sl) + SC[i][1] * (ch + cl) */
+      /* cos(x) ~ SC[i][2] * (ch + cl) - SC[i][1] * (sh + sl) */
+      s_mul (&Sh, &Sl, sgn[neg] * SC[i][2], sh, sl);
+      s_mul (&Ch, &Cl, sgn[neg] * SC[i][1], ch, cl);
+      fast_two_sum (h, l, Ch, Sh);
+      *l += Sl + Cl;
       /* absolute error bounded by 2^-68.588
          from global_error(is_sin=true,rel=false) in sin.sage:
          | h + l - sin2pi (R) | < 2^-68.588
          thus:
          | h + l - sin |x| | < 2^-68.588 + | sin2pi (R) - sin |x| |
                              < 2^-68.588 + err1 */
-      err = 0x1.55p-69; // 2^-66.588 < 0x1.55p-69
+      s_mul (&Ch, &Cl, sgn[neg] * SC[i][2], ch, cl);
+      s_mul (&Sh, &Sl, sgn[neg] * SC[i][1], sh, sl);
+      fast_two_sum (hc, lc, Ch, -Sh);
+      *lc += Cl - Sl;
     }
   else
     {
-      s_mul (&ch, &cl, sgn[neg] * SC[i][2], ch, cl);
-      s_mul (&sh, &sl, sgn[neg] * SC[i][1], sh, sl);
-      fast_two_sum (h, l, ch, -sh);
-      *l += cl - sl;
+      /* sin(x) ~ SC[i][2] * (ch + cl) - SC[i][1] * (sh + sl) */
+      /* cos(x) ~ -SC[i][2] * (sh + sl) - SC[i][1] * (ch + cl) */
+      s_mul (&Ch, &Cl, sgn[neg] * SC[i][2], ch, cl);
+      s_mul (&Sh, &Sl, sgn[neg] * SC[i][1], sh, sl);
+      fast_two_sum (h, l, Ch, -Sh);
+      *l += Cl - Sl;
       /* absolute error bounded by 2^-68.414
          from global_error(is_sin=false,rel=false) in sin.sage:
          | h + l - cos2pi (R) | < 2^-68.414
          thus:
          | h + l - sin |x| | < 2^-68.414 + | cos2pi (R) - sin |x| |
                              < 2^-68.414 + err1 */
-      err = 0x1.81p-69; // 2^-68.414 < 0x1.81p-69
+      s_mul (&Sh, &Sl, sgn[1-neg] * SC[i][2], sh, sl);
+      s_mul (&Ch, &Cl, sgn[1-neg] * SC[i][1], ch, cl);
+      fast_two_sum (hc, lc, Ch, Sh);
+      *lc += Sl + Cl;
     }
-  // *h *= sgn[neg];
-  // *l *= sgn[neg];
+  static const double err = 0x1.81p-69; // maximal error of both cases (see sin.c)
   return err + err1;
 }
 
@@ -2002,8 +2005,172 @@ sin_accurate (double x)
   return y;
 }
 
-double
-cr_sin (double x)
+/* Assume x is a regular number and x > 0x1.6a09e667f3bccp-27. */
+static double
+cos_accurate (double x)
+{
+  dint64_t X[1];
+  dint_fromd (X, x);
+
+  /* reduce argument */
+  reduce (X);
+  
+  // now |X - x/(2pi) mod 1| < 2^-126.67*X, with 0 <= X < 1.
+
+  int neg = 0, is_cos = 1;
+
+  // Write X = i/2^11 + r with 0 <= r < 2^11.
+  int i = reduce2 (X); // exact
+
+  if (i & 0x400) // pi <= x < 2*pi: cos(x) = -cos(x-pi)
+  {
+    neg = 1;
+    i = i & 0x3ff;
+  }
+
+  // now i < 2^10
+
+  if (i & 0x200) // pi/2 <= x < pi: cos(x) = -sin(x-pi/2)
+  {
+    neg = !neg;
+    is_cos = 0;
+    i = i & 0x1ff;
+  }
+
+  // now 0 <= i < 2^9
+
+  if (i & 0x100)
+    // pi/4 <= x < pi/2: cos(x) = sin(pi/2-x), sin(x) = cos(pi/2-x)
+  {
+    is_cos = !is_cos;
+    X->sgn = 1; // negate X
+    add_dint (X, &MAGIC, X); // X -> 2^-11 - X
+    // here: 256 <= i <= 511
+    i = 0x1ff - i;
+    // now 0 <= i < 256
+  }
+
+  // now 0 <= i < 256 and 0 <= X < 2^-11
+
+  /* If is_cos=1, cos |x| = cos2pi (R * (1 + eps))
+        (cases 0 <= x < pi/4 and 3pi/4 <= x < pi)
+     if is_cos=0, cos |x| = sin2pi (R * (1 + eps))
+        (case pi/4 <= x < 3pi/4)
+     In both cases R = i/2^11 + X, 0 <= R < 1/4, and |eps| < 2^-126.67.
+  */
+
+  dint64_t U[1], V[1], X2[1];
+  mul_dint (X2, X, X);       // X2 approximates X^2
+  evalPC (U, X2);    // cos2pi(X)
+  /* since 0 <= X < 2^-11, we have 0.999 < U <= 1 */
+  evalPS (V, X, X2); // sin2pi(X)
+  /* since 0 <= X < 2^-11, we have 0 <= V < 0.0005 */
+  if (!is_cos)
+  {
+    // sin2pi(R) ~ sin2pi(i/2^11)*cos2pi(X)+cos2pi(i/2^11)*sin2pi(X)
+    mul_dint (U, S+i, U);
+    /* since 0 <= S[i] < 0.705 and 0.999 < Uin <= 1, we have
+       0 <= U < 0.705 */
+    mul_dint (V, C+i, V);
+    /* For the error analysis, we distinguish the case i=0.
+       For i=0, we have S[i]=0 and C[1]=1, thus V is the value computed
+       by evalPS() above, with relative error < 2^-124.648.
+
+       For 1 <= i < 256, analyze_sin_case1(rel=true) from sin.sage gives a
+       relative error bound of -122.797 (obtained for i=1).
+       In all cases, the relative error for the computation of
+       sin2pi(i/2^11)*cos2pi(X)+cos2pi(i/2^11)*sin2pi(X) is bounded by -122.797
+       not taking into account the approximation error in R:
+       |U - sin2pi(R)| < |U| * 2^-122.797, with U the value computed
+       after add_dint (U, U, V) below.
+
+       For the approximation error in R, we have:
+       cos(x) = sin2pi (R * (1 + eps))
+       R = i/2^11 + X, 0 <= R < 1/4, and |eps| < 2^-126.67.
+       Thus cos(x) = sin2pi(R+R*eps)
+                   = sin2pi(R)+R*eps*2*pi*cos2pi(theta), theta in [R,R+R*eps]
+       Since 2*pi*R/sin(2*pi*R) < pi/2 for R < 1/4, it follows:
+       | cos(x) - sin2pi(R) | < pi/2*R*|sin(2*pi*R)|
+       | cos(x) - sin2pi(R) | < 2^-126.018 * |sin2pi(R)|.
+
+       Adding both errors we get:
+       | cos(x) - U | < |U| * 2^-122.797 + 2^-126.018 * |sin2pi(R)|
+                      < |U| * 2^-122.797 + 2^-126.018 * |U| * (1 + 2^-122.797)
+                      < |U| * 2^-122.650.
+    */
+  }
+  else
+  {
+    // cos2pi(R) ~ cos2pi(i/2^11)*cos2pi(X)-sin2pi(i/2^11)*sin2pi(X)
+    mul_dint (U, C+i, U);
+    mul_dint (V, S+i, V);
+    V->sgn = 1 - V->sgn; // negate V
+    /* For 0 <= i < 256, analyze_sin_case2(rel=true) from sin.sage gives a
+       relative error bound of -123.540 (obtained for i=0):
+       |U - cos2pi(R)| < |U| * 2^-123.540, with U the value computed
+       after add_dint (U, U, V) below.
+
+       For the approximation error in R, we have:
+       cos(x) = cos2pi (R * (1 + eps))
+       R = i/2^11 + X, 0 <= R < 1/4, and |eps| < 2^-126.67.
+       Thus cos(x) = cos2pi(R+R*eps)
+                   = cos2pi(R)-R*eps*2*pi*sin2pi(theta), theta in [R,R+R*eps]
+       Since we have R < 1/4, we have cos2pi(R) >= sqrt(2)/2,
+       and it follows:
+       | cos(x)/cos2pi(R) - 1 | < 2*pi*R*eps/(sqrt(2)/2)
+                                < pi/2*eps/sqrt(2)          [since R < 1/4]
+                                < 2^-126.518.
+       Adding both errors we get:
+       | cos(x) - U | < |U| * 2^-123.540 + 2^-126.518 * |cos2pi(R)|
+                      < |U| * 2^-123.540 + 2^-126.518 * |U| * (1 + 2^-123.540)
+                      < |U| * 2^-123.367.
+    */
+  }
+  add_dint (U, U, V);
+  /* If is_cos=0:
+     | cos(x) - U | < |U| * 2^-122.650
+     If is_cos=1:
+     | cos(x) - U | < |U| * 2^-123.367.
+     In all cases the total error is bounded by |U| * 2^-122.650.
+     The term |U| * 2^-122.650 contributes to at most 2^(128-122.650) < 41 ulps
+     relatively to U->lo.
+  */
+  uint64_t err = 41;
+  uint64_t hi0, hi1, lo0, lo1;
+  lo0 = U->lo - err;
+  hi0 = U->hi - (lo0 > U->lo);
+  lo1 = U->lo + err;
+  hi1 = U->hi + (lo1 < U->lo);
+  /* check the upper 54 bits are equal */
+  if ((hi0 >> 10) != (hi1 >> 10))
+    {
+      static const double exceptions[][3] = {
+        {0x1.8000000000009p-23, 0x1.fffffffffff7p-1, 0x1.b56666666666cp-143},
+        {0x1.8000000000024p-22, 0x1.ffffffffffdcp-1, 0x1.b56666666667ep-137},
+        {0x1.800000000009p-21,  0x1.ffffffffff7p-1,  0x1.b5666666666c4p-131},
+        {0x1.20000000000f3p-20, 0x1.fffffffffebcp-1, 0x1.37642666666fdp-127},
+        {0x1.800000000024p-20,  0x1.fffffffffdcp-1,  0x1.b5666666667ddp-125},
+      };
+      for (int k = 0; k < 5; k++)
+        {
+          if (__builtin_fabs (x) == exceptions[k][0])
+            return exceptions[k][1] + exceptions[k][2];
+        }
+      printf ("Rounding test of accurate path failed for cos(%la)\n", x);
+      printf ("Please report the above to core-math@inria.fr\n");
+      exit (1);
+    }
+
+  if (neg)
+    U->sgn = 1 - U->sgn;
+
+  double y = dint_tod (U);
+
+  return y;
+}
+
+void
+cr_sincos (double x, double *s, double *c)
 {
   b64u64_u t = {.f = x};
   int e = (t.u >> 52) & 0x7ff;
@@ -2011,36 +2178,45 @@ cr_sin (double x)
   if (__builtin_expect (e == 0x7ff, 0)) /* NaN, +Inf and -Inf. */
     {
       t.u = ~0ull;
-      return t.f;
+      *s = t.f;
+      *c = t.f;
+      return;
     }
 
   /* now x is a regular number */
 
-  /* For |x| <= 0x1.7137449123ef6p-26, sin(x) rounds to x (to nearest):
-     we can assume x >= 0 without loss of generality since sin(-x) = -sin(x),
-     we have x - x^3/6 < sin(x) < x for say 0 < x <= 1 thus
-     |sin(x) - x| < x^3/6.
-     Write x = c*2^e with 1/2 <= c < 1.
-     Then ulp(x)/2 = 2^(e-54), and x^3/6 = c^3/6*2^(3e), thus
-     x^3/6 < ulp(x)/2 rewrites as c^3/6*2^(3e) < 2^(e-54),
-     or c^3*2^(2e+53) < 3 (1).
-     For e <= -26, since c^3 < 1, we have c^3*2^(2e+53) < 2 < 3.
-     For e=-25, (1) rewrites 8*c^3 < 3 which yields c <= 0x1.7137449123ef6p-1.
-  */
+  /* For |x| <= 0x1.6a09e667f3bccp-27, sin(x) round to x and cos(x) rounds to 1
+     (to nearest) */
   uint64_t ux = t.u & 0x7fffffffffffffff;
-  // 0x3e57137449123ef6 = 0x1.7137449123ef6p-26
-  if (ux <= 0x3e57137449123ef6)
+  if (ux <= 0x3e46a09e667f3bccull) // |x| <= 0x1.6a09e667f3bccp-27
+  {
     // Taylor expansion of sin(x) is x - x^3/6 around zero
     // for x=-0, fma (x, -0x1p-54, x) returns +0
-    return (x == 0) ? x : __builtin_fma (x, -0x1p-54, x);
+    *s = (x == 0) ? x : __builtin_fma (x, -0x1p-54, x);
+    *c = (x == 0) ? 1.0 : 1.0 - 0x1p-54;
+    return;
+  }
 
-  double h, l, err;
-  err = sin_fast (&h, &l, x);
-  double left  = h + (l - err), right = h + (l + err);
-  /* With SC[] from ./buildSC 15 we get 1100 failures out of 50000000
-     random tests, i.e., about 0.002%. */
-  if (__builtin_expect (left == right, 1))
-    return left;
+  // now |x| > 0x1.6a09e667f3bccp-27
+  double h, l, hc, lc, err;
+  err = sincos_fast (&h, &l, &hc, &lc, x);
+  *s  = h + (l - err);
+  double right = h + (l + err);
+  *c  = hc + (lc - err);
+  double right_c = hc + (lc + err);
+  if (__builtin_expect (*s == right && *c == right_c, 1))
+    return;
 
-  return sin_accurate (x);
+  if (*c == right_c) // fast path succeeded for cos
+  {
+    *s = sin_accurate (x);
+    return;
+  }
+
+  *c = cos_accurate (x);
+
+  if (*s == right) // fast path succeeded for sin
+    return;
+
+  *s = sin_accurate (x);
 }
