7835c5d9ff91d92c1d129b38c351d5e4bbd85864
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2024-11-21 16:05:15 +0100

    [atan2f] fix missing invalid exception for y=qnan and x=snan
    (reported by Adhemerval Zanella during GNU libc integration)

--- src/binary32/atan2/atan2f.c
+++ src/binary32/atan2/atan2f.c
@@ -100,9 +100,11 @@ float cr_atan2f(float y, float x){
   static const double sgn[] = {1,-1};
   b32u32_u tx = {.f = x}, ty = {.f = y};
   uint32_t ux = tx.u, uy = ty.u, ax = ux&(~0u>>1), ay = uy&(~0u>>1);
-  if(__builtin_expect(ay >= (0xff<<23)||ax >= (0xff<<23), 0)){
-    if(ay > (0xff<<23)) return y + y; // nan
-    if(ax > (0xff<<23)) return x + x; // nan
+  if(__builtin_expect(ay >= (0xff<<23)||ax >= (0xff<<23), 0)){ // x or y is nan or inf
+    /* we use x+y below so that the invalid exception is set
+       for (x,y) = (qnan,snan) or (snan,qnan) */
+    if(ay > (0xff<<23)) return x + y; // case y nan
+    if(ax > (0xff<<23)) return x + y; // case x nan
     uint32_t yinf = ay==(0xff<<23), xinf = ax==(0xff<<23);
     if(yinf&xinf){
       if(ux>>31)
