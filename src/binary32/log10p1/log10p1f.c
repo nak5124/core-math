2870fbf96ec1ac08f46f9ccc370248f43277522b
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2025-01-06 15:51:05 +0100

    [log10p1] errno support

--- src/binary32/log10p1/log10p1f.c
+++ src/binary32/log10p1/log10p1f.c
@@ -53,12 +53,16 @@ static __attribute__((noinline)) float as_special(float x){
   if(ux == 0x7f800000u) return x; // +inf
   uint32_t ax = ux<<1;
   if(ax == 0x17fu<<24) { // x+1 = 0.0
+#ifdef CORE_MATH_SUPPORT_ERRNO
     errno = ERANGE;
+#endif
     feraiseexcept(FE_DIVBYZERO);
     return -__builtin_inff(); // -inf
   }
   if(ax > 0xff000000u) return x + x; // nan
+#ifdef CORE_MATH_SUPPORT_ERRNO
   errno = EDOM;
+#endif
   feraiseexcept(FE_INVALID);
   return get_nan (); // x < 0
 }
