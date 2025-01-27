72eff8be672032cdde735811b5dceb77e024824c
Alexei Sibidanov <sibid@uvic.ca>
2025-01-27 12:09:53 -0800

    [exp] add argument range to test performance of denormal results in comments

--- src/generic/exp/random_under_test.h
+++ src/generic/exp/random_under_test.h
@@ -2,4 +2,7 @@ static inline TYPE_UNDER_TEST random_under_test (void)
 {
   /* sample in [-10,10] */
   return 20 * ((TYPE_UNDER_TEST) rand() / (TYPE_UNDER_TEST) RAND_MAX) - 10;
+
+  /* for performance test of denormal results in the binary64 format */
+  /* return -0x1.205966f2b4f1p+5*((TYPE_UNDER_TEST) rand() / (TYPE_UNDER_TEST) RAND_MAX) - 0x1.6232bdd7abcd2p+9; */
 }
