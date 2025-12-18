1936563df13181fe65aeca48149b9068f6656e37
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2025-12-18 02:45:39 +0100

    allow XMIN/XMAX for asinh

--- src/generic/asinh/random_under_test.h
+++ src/generic/asinh/random_under_test.h
@@ -1,5 +1,14 @@
+/* sample by default in [-10,10] */
+
+#ifndef XMIN
+#define XMIN -10
+#endif
+
+#ifndef XMAX
+#define XMAX 10
+#endif
+
 static inline TYPE_UNDER_TEST random_under_test (void)
 {
-  /* sample in [-10,10] */
-  return 20 * ((double) rand() / (double) RAND_MAX) - 10;
+  return XMIN + (XMAX - XMIN) * ((double) rand() / (double) RAND_MAX);
 }
