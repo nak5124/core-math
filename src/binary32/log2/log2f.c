7cb9f87c2c23b4512f888a31968fb0d2ab5a5d2e
Alexei Sibidanov <sibid@uvic.ca>
2026-01-06 17:55:36 -0800

    [log2f] reduce lookup table in two times and get rid of a fma requirement in the range reduction

--- src/binary32/log2/log2f.c
+++ src/binary32/log2/log2f.c
@@ -1,6 +1,6 @@
 /* Correctly-rounded binary logarithm function for binary32 value.
 
-Copyright (c) 2022 Alexei Sibidanov.
+Copyright (c) 2022-2026 Alexei Sibidanov <sibid@uvic.ca>
 
 This file is part of the CORE-MATH project
 (https://core-math.gitlabpages.inria.fr/).
@@ -37,114 +37,88 @@ SOFTWARE.
 typedef union {float f; uint32_t u;} b32u32_u;
 typedef union {double f; uint64_t u;} b64u64_u;
 
+static __attribute__((noinline)) float as_special(float x){
+  b32u32_u t = {.f = x};
+  uint32_t ux = t.u, ax = ux<<1;
+  if(ax == 0u){ // +/-0.0
+#ifdef CORE_MATH_SUPPORT_ERRNO
+    errno = ERANGE;
+#endif
+    return -1.0f/0.0f; // to raise FE_DIVBYZERO
+  }
+  if(ux == 0x7f800000u) return x; // +inf
+  if(ax > 0xff000000u) return x + x; // nan
+#ifdef CORE_MATH_SUPPORT_ERRNO
+  errno = EDOM;
+#endif
+  return 0.0f/0.0f; // to raise FE_INVALID and return nan
+}
+
 float cr_log2f(float x) {
   static const double ix[] = {
-    0x1p+0, 0x1.fc07f01fcp-1, 0x1.f81f81f82p-1, 0x1.f44659e4ap-1, 0x1.f07c1f07cp-1,
-    0x1.ecc07b302p-1, 0x1.e9131abfp-1, 0x1.e573ac902p-1, 0x1.e1e1e1e1ep-1,
-    0x1.de5d6e3f8p-1, 0x1.dae6076bap-1, 0x1.d77b654b8p-1, 0x1.d41d41d42p-1,
-    0x1.d0cb58f6ep-1, 0x1.cd8568904p-1, 0x1.ca4b3055ep-1, 0x1.c71c71c72p-1,
-    0x1.c3f8f01c4p-1, 0x1.c0e070382p-1, 0x1.bdd2b8994p-1, 0x1.bacf914c2p-1,
-    0x1.b7d6c3ddap-1, 0x1.b4e81b4e8p-1, 0x1.b2036406cp-1, 0x1.af286bca2p-1,
-    0x1.ac5701ac6p-1, 0x1.a98ef606ap-1, 0x1.a6d01a6dp-1, 0x1.a41a41a42p-1,
-    0x1.a16d3f97ap-1, 0x1.9ec8e951p-1, 0x1.9c2d14ee4p-1, 0x1.99999999ap-1,
-    0x1.970e4f80cp-1, 0x1.948b0fcd6p-1, 0x1.920fb49dp-1, 0x1.8f9c18f9cp-1,
-    0x1.8d3018d3p-1, 0x1.8acb90f6cp-1, 0x1.886e5f0acp-1, 0x1.861861862p-1,
-    0x1.83c977ab2p-1, 0x1.818181818p-1, 0x1.7f405fd02p-1, 0x1.7d05f417ep-1,
-    0x1.7ad2208ep-1, 0x1.78a4c8178p-1, 0x1.767dce434p-1, 0x1.745d1745ep-1,
-    0x1.724287f46p-1, 0x1.702e05c0cp-1, 0x1.6e1f76b44p-1, 0x1.6c16c16c2p-1,
-    0x1.6a13cd154p-1, 0x1.681681682p-1, 0x1.661ec6a52p-1, 0x1.642c8590cp-1,
-    0x1.623fa7702p-1, 0x1.605816058p-1, 0x1.5e75bb8dp-1, 0x1.5c9882b94p-1,
-    0x1.5ac056b02p-1, 0x1.58ed23082p-1, 0x1.571ed3c5p-1, 0x1.555555556p-1,
-    0x1.5390948f4p-1, 0x1.51d07eae2p-1, 0x1.501501502p-1, 0x1.4e5e0a73p-1,
-    0x1.4cab88726p-1, 0x1.4afd6a052p-1, 0x1.49539e3b2p-1, 0x1.47ae147aep-1,
-    0x1.460cbc7f6p-1, 0x1.446f86562p-1, 0x1.42d6625d6p-1, 0x1.414141414p-1,
-    0x1.3fb013fbp-1, 0x1.3e22cbce4p-1, 0x1.3c995a47cp-1, 0x1.3b13b13b2p-1,
-    0x1.3991c2c18p-1, 0x1.381381382p-1, 0x1.3698df3dep-1, 0x1.3521cfb2cp-1,
-    0x1.33ae45b58p-1, 0x1.323e34a2cp-1, 0x1.30d19013p-1, 0x1.2f684bda2p-1,
-    0x1.2e025c04cp-1, 0x1.2c9fb4d82p-1, 0x1.2b404ad02p-1, 0x1.29e4129e4p-1,
-    0x1.288b01288p-1, 0x1.27350b882p-1, 0x1.25e22708p-1, 0x1.24924924ap-1,
-    0x1.23456789ap-1, 0x1.21fb78122p-1, 0x1.20b470c68p-1, 0x1.1f7047dc2p-1,
-    0x1.1e2ef3b4p-1, 0x1.1cf06ada2p-1, 0x1.1bb4a4046p-1, 0x1.1a7b9611ap-1,
-    0x1.19453808cp-1, 0x1.181181182p-1, 0x1.16e068942p-1, 0x1.15b1e5f76p-1,
-    0x1.1485f0e0ap-1, 0x1.135c81136p-1, 0x1.12358e75ep-1, 0x1.111111112p-1,
-    0x1.0fef010fep-1, 0x1.0ecf56be6p-1, 0x1.0db20a89p-1, 0x1.0c9714fbcp-1,
-    0x1.0b7e6ec26p-1, 0x1.0a6810a68p-1, 0x1.0953f3902p-1, 0x1.084210842p-1,
-    0x1.073260a48p-1, 0x1.0624dd2f2p-1, 0x1.05197f7d8p-1, 0x1.041041042p-1,
-    0x1.03091b52p-1, 0x1.020408102p-1, 0x1.01010101p-1, 0x1p-1 };
+    0x1p+0, 0x1.f81f82p-1, 0x1.f07c1fp-1, 0x1.e9131acp-1,
+    0x1.e1e1e1ep-1, 0x1.dae6077p-1, 0x1.d41d41dp-1, 0x1.cd85689p-1,
+    0x1.c71c71cp-1, 0x1.c0e0704p-1, 0x1.bacf915p-1, 0x1.b4e81b5p-1,
+    0x1.af286bdp-1, 0x1.a98ef6p-1, 0x1.a41a41ap-1, 0x1.9ec8e95p-1,
+    0x1.999999ap-1, 0x1.948b0fdp-1, 0x1.8f9c19p-1, 0x1.8acb90fp-1,
+    0x1.8618618p-1, 0x1.8181818p-1, 0x1.7d05f41p-1, 0x1.78a4c81p-1,
+    0x1.745d174p-1, 0x1.702e05cp-1, 0x1.6c16c17p-1, 0x1.6816817p-1,
+    0x1.642c859p-1, 0x1.605816p-1, 0x1.5c9882cp-1, 0x1.58ed231p-1,
+    0x1.5555555p-1, 0x1.51d07ebp-1, 0x1.4e5e0a7p-1, 0x1.4afd6ap-1,
+    0x1.47ae148p-1, 0x1.446f865p-1, 0x1.4141414p-1, 0x1.3e22cbdp-1,
+    0x1.3b13b14p-1, 0x1.3813814p-1, 0x1.3521cfbp-1, 0x1.323e34ap-1,
+    0x1.2f684bep-1, 0x1.2c9fb4ep-1, 0x1.29e412ap-1, 0x1.27350b9p-1,
+    0x1.2492492p-1, 0x1.21fb781p-1, 0x1.1f7047ep-1, 0x1.1cf06aep-1,
+    0x1.1a7b961p-1, 0x1.1811812p-1, 0x1.15b1e5fp-1, 0x1.135c811p-1,
+    0x1.1111111p-1, 0x1.0ecf56cp-1, 0x1.0c9715p-1, 0x1.0a6810ap-1,
+    0x1.0842108p-1, 0x1.0624dd3p-1, 0x1.041041p-1, 0x1.0204081p-1, 0.5};
 
   static const double lix[] = {
-    0x0p+0,
-    -0x1.6fe50b6f1eafap-7, -0x1.6e79685c160d5p-6, -0x1.11cd1d51955bap-5, -0x1.6bad37591e03p-5,
-    -0x1.c4dfab908ddb6p-5, -0x1.0eb389fab4795p-4, -0x1.3aa2fdd26ae99p-4, -0x1.663f6faca846bp-4,
-    -0x1.918a16e4cb157p-4, -0x1.bc84240a78a14p-4, -0x1.e72ec1181cfbp-4, -0x1.08c588cd964e4p-3,
-    -0x1.1dcd19759f2e3p-3, -0x1.32ae9e27627c6p-3, -0x1.476a9f989a58ap-3, -0x1.5c01a39fa6533p-3,
-    -0x1.70742d4eed455p-3, -0x1.84c2bd02d6433p-3, -0x1.98edd077e9f09p-3, -0x1.acf5e2db31eeap-3,
-    -0x1.c0db6cddaa82dp-3, -0x1.d49ee4c331219p-3, -0x1.e840be751d776p-3, -0x1.fbc16b9003e0cp-3,
-    -0x1.0790adbae3fcp-2, -0x1.11307dad465b5p-2, -0x1.1ac05b2924cc6p-2, -0x1.24407ab0cc40fp-2,
-    -0x1.2db10fc4ea424p-2, -0x1.37124cea58697p-2, -0x1.406463b1d455dp-2, -0x1.49a784bcbaa37p-2,
-    -0x1.52dbdfc4f341cp-2, -0x1.5c01a39ff2c9bp-2, -0x1.6518fe46abaa5p-2, -0x1.6e221cd9d6934p-2,
-    -0x1.771d2ba7f5791p-2, -0x1.800a56315ee29p-2, -0x1.88e9c72df8611p-2, -0x1.91bba891d495fp-2,
-    -0x1.9a8023920fa4dp-2, -0x1.a33760a7fbca6p-2, -0x1.abe18797d2effp-2, -0x1.b47ebf734b923p-2,
-    -0x1.bd0f2e9eb2b84p-2, -0x1.c592fad2be1abp-2, -0x1.ce0a4923cf5e6p-2, -0x1.d6753e02f4ebbp-2,
-    -0x1.ded3fd445afp-2, -0x1.e726aa1e558fep-2, -0x1.ef6d67325ba37p-2, -0x1.f7a8568c8aea5p-2,
-    -0x1.ffd799a81be86p-2, 0x1.f804ae8d33c41p-2, 0x1.efec61b04af4ep-2, 0x1.e7df5fe572607p-2,
-    0x1.dfdd89d5b0009p-2, 0x1.d7e6c0abbd923p-2, 0x1.cffae611a74d6p-2, 0x1.c819dc2d8578dp-2,
-    0x1.c043859e5bdbcp-2, 0x1.b877c57b47c04p-2, 0x1.b0b67f4f29a67p-2, 0x1.a8ff97183ed06p-2,
-    0x1.a152f14293c74p-2, 0x1.99b072a9289cap-2, 0x1.921800927e284p-2, 0x1.8a8980ac4113p-2,
-    0x1.8304d90c2859dp-2, 0x1.7b89f02cbd499p-2, 0x1.7418aceb84ab1p-2, 0x1.6cb0f68656c94p-2,
-    0x1.6552b49993dc2p-2, 0x1.5dfdcf1eacd7bp-2, 0x1.56b22e6b97c17p-2, 0x1.4f6fbb2ce6943p-2,
-    0x1.48365e6957b42p-2, 0x1.4106017c0dbcfp-2, 0x1.39de8e15727d9p-2, 0x1.32bfee37489bbp-2,
-    0x1.2baa0c34989c3p-2, 0x1.249cd2b177fd5p-2, 0x1.1d982c9d50468p-2, 0x1.169c0536677acp-2,
-    0x1.0fa848045f67bp-2, 0x1.08bce0d9a7c6p-2, 0x1.01d9bbcf66a2cp-2, 0x1.f5fd8a90e2d85p-3,
-    0x1.e857d3d3af1e5p-3, 0x1.dac22d3ec5f4ep-3, 0x1.cd3c712db459ap-3, 0x1.bfc67a7ff3c21p-3,
-    0x1.b2602497678f4p-3, 0x1.a5094b555a1f9p-3, 0x1.97c1cb136b96fp-3, 0x1.8a8980ac8652dp-3,
-    0x1.7d60496c83f67p-3, 0x1.7046031c7cdbp-3, 0x1.633a8bf460335p-3, 0x1.563dc2a08b102p-3,
-    0x1.494f863bbc1ddp-3, 0x1.3c6fb6507a37ep-3, 0x1.2f9e32d5257ecp-3, 0x1.22dadc2a627efp-3,
-    0x1.1625931802e49p-3, 0x1.097e38cef9519p-3, 0x1.f9c95dc138295p-4, 0x1.e0b1ae90505f6p-4,
-    0x1.c7b528b5fcffap-4, 0x1.aed391abb17a1p-4, 0x1.960caf9bd35eap-4, 0x1.7d60496e3edebp-4,
-    0x1.64ce26bf2108ep-4, 0x1.4c560fe5b573bp-4, 0x1.33f7cde24adfbp-4, 0x1.1bb32a5ed9353p-4,
-    0x1.0387efbd3006ep-4, 0x1.d6ebd1f1d0956p-5, 0x1.a6f9c37a8beacp-5, 0x1.77394c9d6762cp-5,
-    0x1.47aa07358e1a4p-5, 0x1.184b8e4d490efp-5, 0x1.d23afc4d95c78p-6, 0x1.743ee8678a7cap-6,
-    0x1.16a21e243bf77p-6, 0x1.72c7ba20c907ep-7, 0x1.720d9c0536e16p-8, 0x0p+0};
-    
+    0x0p+0, -0x1.6e7966ead8ac5p-6, -0x1.6bad38119a13ap-5, -0x1.0eb389ee9f56p-4,
+    -0x1.663f6fc3a678dp-4, -0x1.bc8423d408321p-4, -0x1.08c588e79f8e9p-3, -0x1.32ae9e28fc362p-3,
+    -0x1.5c01a3cde7f74p-3, -0x1.84c2bccf005a9p-3, -0x1.acf5e2c156d8fp-3, -0x1.d49ee4b90c47cp-3,
+    -0x1.fbc16b67c143bp-3, -0x1.11307dc445fecp-2, -0x1.24407abf4dc03p-2, -0x1.37124cede831fp-2,
+    -0x1.49a784a5bc715p-2, -0x1.5c01a3965cc38p-2, -0x1.6e221cc2bb868p-2, -0x1.800a564aa10b6p-2,
+    -0x1.91bba8a906b7fp-2, -0x1.a33760adbb56ep-2, -0x1.b47ebf91d417cp-2, -0x1.c592faf028f8ep-2,
+    -0x1.d6753e1a43e85p-2, -0x1.e726aa2157f61p-2, -0x1.f7a8567cd1cbdp-2, -0x1.03fda8a93ea1cp-1,
+    -0x1.0c10500ed4feep-1, -0x1.140c9fb5a8f7fp-1, -0x1.1bf311daefb44p-1, -0x1.23c41d317edc1p-1,
+    -0x1.2b80347f8250cp-1, -0x1.3327c6a752222p-1, -0x1.3abb3fb080128p-1, -0x1.423b07f5114e5p-1,
+    -0x1.49a784b14715p-1, -0x1.5101187e9b871p-1, -0x1.5848226c6c7c3p-1, -0x1.5f7cff3de8f2ap-1,
+    -0x1.66a008d8ede91p-1, -0x1.6db19694a04aap-1, -0x1.74b1fd6b5e715p-1, -0x1.7ba18f99ce2a5p-1,
+    -0x1.82809d4d79ba9p-1, -0x1.894f749cf5047p-1, -0x1.900e615bac2f7p-1, -0x1.96bdad16f516p-1,
+    -0x1.9d5d9fe08baefp-1, -0x1.a3ee7f3e4a7ebp-1, -0x1.aa708f4de7fep-1, -0x1.b0e4125ca68fep-1,
+    -0x1.b74948f9a72bp-1, -0x1.bda071b77c9e2p-1, -0x1.c3e9ca4193f99p-1, -0x1.ca258dd397814p-1,
+    -0x1.d053f6d543325p-1, -0x1.d6753dfedaa39p-1, -0x1.dc899aa874b31p-1, -0x1.e29142f2e8b3dp-1,
+    -0x1.e88c6b41b14aep-1, -0x1.ee7b4718b4414p-1, -0x1.f45e08c87b09p-1, -0x1.fa34e117d8785p-1,
+    -0x1p+0
+  };
+  static const double b[] = {
+    0x1.7154765bab2b5p+0, -0x1.71574d6939f6cp-1, 0x1.ec60b584ca47fp-2};
+  static const double c[] = {
+    0x1.71547652b8314p+0, -0x1.71547652b7f67p-1, 0x1.ec709db872c63p-2,
+    -0x1.715476b064cdfp-2, 0x1.277c72c15441fp-2, -0x1.ec4ff35b64c31p-3};
+
   b32u32_u t = {.f = x};
   uint32_t ux = t.u;
-  uint64_t m = ux&(~0u>>9); m <<= 52-23;
-  int e = (ux>>23) - 0x7f;
-  if (__builtin_expect(ux < 1u<<23 || ux >= 0xffu<<23, 0)) {
-    if (ux==0||ux==(1u<<31)) { // x = +/-0
-#ifdef CORE_MATH_SUPPORT_ERRNO
-      errno = ERANGE;
-#endif
-      return -1.0f / 0.0f; // should raise 'Divide by zero' exception.
-    }
-    uint32_t inf_or_nan = ((ux>>23)&0xff) == 0xff, nan = inf_or_nan && (ux<<9);
-    if (ux>>31 && !nan) { // x < 0
-#ifdef CORE_MATH_SUPPORT_ERRNO
-      errno = EDOM;
-#endif
-      return (x - x) / (x - x);  // should raise 'Invalid operation' exception.
-    }
-    if (inf_or_nan) return x + x;
-    // subnormal
-    int nz = __builtin_clzll(m);
-    m <<= nz-11;
-    m &= ~(uint64_t)0>>12;
-    e -= nz-12;
+  if(__builtin_expect(ux >= 0x7f800000u, 0)) return as_special(x); // <=-0, nan, inf
+  if(__builtin_expect(ux<(1u<<23), 0)){ // subnormal
+    if(__builtin_expect(ux == 0u, 0)) return as_special(x); // +0
+    int n = __builtin_clz(ux) - 8;
+    ux <<= n;
+    ux -= n<<23;
   }
+  int e = ((int32_t)ux>>23) - 0x7f;
+  uint32_t m = ux&(~0u>>9);
   if(__builtin_expect(!m, 0)) return e;
-  int j = (m + ((int64_t)1<<(52-8)))>>(52-7), k = j>53;
-  e += k;
-  b64u64_u xd = {.u = m | (uint64_t)0x3ff<<52};
-  double z = __builtin_fma(xd.f, ix[j], -1.0); // z is exact
-  // the coefficients of c are pre-multiplied by 1/log(2)
-  static const double c[] =
-    {0x1.71547652b82fep+0, -0x1.71547652b82fep-1, 0x1.ec709dc32988ap-2,
-     -0x1.715476521ec2cp-2, 0x1.277801a1ad904p-2, -0x1.ec731704d6a88p-3};
-  double z2 = z*z;
+  int32_t j = (m + (1<<(23-7)))>>(23-6);
+  b64u64_u xd = {.u = (uint64_t)m<<(52-23) | 0x3ffull<<52};
+  double z = xd.f*ix[j] - 1.0, z2 = z*z, el = e - lix[j];
+  double f = (el + z*b[0]) + z2*(b[1] + z*b[2]);
+  float lb = f , ub = f + 0x1.661p-32;
+  if(__builtin_expect(lb==ub, 1)) return lb;
   double c0 = c[0] + z*c[1];
   double c2 = c[2] + z*c[3];
   double c4 = c[4] + z*c[5];
   c0 += z2*(c2 + z2*c4);
-  return  z*c0 + (e - lix[j]);
+  return el + z*c0;
 }
