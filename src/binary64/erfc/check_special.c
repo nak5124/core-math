846cd0a53d3bf2768325706955b20b4a79acd304
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2026-01-13 15:36:10 +0100

    [erfc] added scan_consecutive test

--- src/binary64/erfc/check_special.c
+++ src/binary64/erfc/check_special.c
@@ -32,9 +32,11 @@ SOFTWARE.
 #include <math.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <mpfr.h>
 #if (defined(_OPENMP) && !defined(CORE_MATH_NO_OPENMP))
 #include <omp.h>
 #endif
+#include "function_under_test.h"
 
 #ifndef CORE_MATH_TESTS
 #define CORE_MATH_TESTS 1000000000UL /* total number of tests */
@@ -50,6 +52,11 @@ int rnd1[] = { FE_TONEAREST, FE_TOWARDZERO, FE_UPWARD, FE_DOWNWARD };
 
 int rnd = 0;
 int verbose = 0;
+unsigned long tested = 0;
+
+static inline double tfun(double x){
+  return cr_function_under_test(x);
+}
 
 #define MAX_THREADS 192
 
@@ -92,6 +99,8 @@ get_random (int tid)
 static void
 check (double x)
 {
+#pragma omp atomic update
+  tested ++;
   int bug;
   double y1 = ref_erfc (x);
   fesetround (rnd1[rnd]);
@@ -136,9 +145,102 @@ check_subnormal (void)
   }
 }
 
+// put in h+l a double-double approximation of erfc(x)
+static void
+dd_erfc (double *h, double *l, double x)
+{
+  mpfr_t t;
+  mpfr_init2 (t, 107);
+  mpfr_set_d (t, x, MPFR_RNDN);
+  mpfr_erfc (t, t, MPFR_RNDN);
+  *h = mpfr_get_d (t, MPFR_RNDN);
+  mpfr_sub_d (t, t, *h, MPFR_RNDN);
+  *l = mpfr_get_d (t, MPFR_RNDN);
+  mpfr_clear (t);
+}
+
+// return a double approximation of the derivative of erfc(x)
+static double
+deriv (double x)
+{
+  mpfr_t t;
+  // the derivative of erfc(x) is -2*e^(-x^2)/sqrt(pi)
+  mpfr_init2 (t, 53);
+  mpfr_set_d (t, x, MPFR_RNDN);
+  mpfr_sqr (t, t, MPFR_RNDN); // t = x^2
+  mpfr_neg (t, t, MPFR_RNDN); // t = -x^2
+  mpfr_exp (t, t, MPFR_RNDN); // t = exp(-x^2)
+  double ret = mpfr_get_d (t, MPFR_RNDN);
+  mpfr_clear (t);
+  return -0x1.20dd750429b6dp+0 * ret;
+}
+
+static void scan_consecutive_aux(int64_t n, double x){
+  ref_init();
+  ref_fesetround(rnd);
+  fesetround(rnd1[rnd]);
+  while (n) {
+    double h, l, d, dd;
+    dd_erfc (&h, &l, x);
+    int e;
+    frexp (x, &e);
+    d = deriv (x); // derivative of erfc(x)
+    dd = fabs (-2.0 * x * d); // absolute value of 2nd derivative
+    /* 2^(e-1) <= |x| < 2^e thus ulp(x) = 2^(e-53) */
+    d = ldexp (d, e - 53); // multiply d by ulp(x)
+    dd = ldexp (dd, 2 * (e - 53)); // multiply dd by ulp(x)^2
+    /* we want j^2*dd < 2^-11 ulp(h) so that the 2nd-order term
+       produces an error bounded by 2^-11 ulp(h), to that MPFR
+       will be called with probability about 2^-11.
+       Thus approximately j^2*dd < 2^-64 h,
+       or j < 2^-32 sqrt(h/dd) */
+    int64_t jmax = 0x1p-32 * sqrt (h / dd);
+    if (jmax > n) jmax = n; // cap to n
+    if (jmax == 0) jmax = 1; // ensure progress
+    for(int64_t j=0;j<jmax;j++){
+      b64u64_u v = {.f = x};
+      v.u += j;
+      double t = tfun (v.f);
+      // erfc(x+j*u) is approximated by h + l + j*d
+      double w = h + __builtin_fma (j, d, l);
+      if (t != w) // expensive test
+        check(v.f);
+    }
+    n -= jmax;
+    x += jmax * ldexp (1.0, e - 53);
+  }
+}
+
+static void scan_consecutive (int64_t n, double x){
+  int nthreads = 1;
+  if (n < 0) {
+    n = -n;
+    x = asfloat64 (asuint64 (x) - n);
+  }
+#if (defined(_OPENMP) && !defined(CORE_MATH_NO_OPENMP))
+#pragma omp parallel
+  nthreads = omp_get_num_threads ();
+#endif
+  int64_t h = (n - 1) / nthreads + 1; // ceil(n/nthreads)
+#if (defined(_OPENMP) && !defined(CORE_MATH_NO_OPENMP))
+#pragma omp parallel for schedule(static,1)
+#endif
+  for (int i = 0; i < nthreads; i++) {
+    int64_t ni = i * h;
+    double xi = asfloat64 (asuint64 (x) + ni);
+    int64_t hi = (ni + h > n) ? n - ni : h;
+    scan_consecutive_aux (hi, xi);
+  }
+  printf ("checked %lu values, expensive checks %lu\n", n, tested);
+}
+
 int
 main (int argc, char *argv[])
 {
+  int conseq = 0;
+  unsigned long n = 1000000; // length of consecutive runs with -C
+  double a = 1.5;  // starting point of consecutive runs with -C
+
   while (argc >= 2)
     {
       if (strcmp (argv[1], "--rndn") == 0)
@@ -165,6 +267,19 @@ main (int argc, char *argv[])
           argc --;
           argv ++;
         }
+      else if (strcmp (argv[1], "-C") == 0)
+        {
+          conseq = 1;
+          n = strtoul (argv[2], NULL, 0);
+          argc -= 2;
+          argv += 2;
+        }
+      else if (strcmp (argv[1], "-a") == 0)
+        {
+          a = strtod (argv[2], NULL);
+          argc -= 2;
+          argv += 2;
+        }
       else if (strcmp (argv[1], "--verbose") == 0)
         {
           verbose = 1;
@@ -177,6 +292,11 @@ main (int argc, char *argv[])
           exit (1);
         }
     }
+
+  if (conseq) {
+    scan_consecutive (n, a);
+    return 0;
+  }
 
   check_subnormal ();
 
@@ -191,7 +311,7 @@ main (int argc, char *argv[])
 #if (defined(_OPENMP) && !defined(CORE_MATH_NO_OPENMP))
 #pragma omp parallel for
 #endif
-  for (uint64_t n = 0; n < CORE_MATH_TESTS; n++)
+  for (n = 0; n < CORE_MATH_TESTS; n++)
   {
     ref_init ();
     ref_fesetround (rnd);
