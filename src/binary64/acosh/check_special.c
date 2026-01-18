3b64fe128fcb06229fc78664497b75b7b3047932
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2026-01-18 07:59:26 +0100

    [acosh] reduce number of special checks

--- src/binary64/acosh/check_special.c
+++ src/binary64/acosh/check_special.c
@@ -238,7 +238,7 @@ static void check_val(double x){
 }
 
 #ifndef CORE_MATH_TESTS
-#define CORE_MATH_TESTS 1000000000UL /* total number of tests */
+#define CORE_MATH_TESTS 500000000UL /* total number of tests */
 #endif
 
 static void check_random_all(int seed, double a, double b){
