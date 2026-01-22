f1d48f55a598c32476c73970183383d6f3fc437a
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2026-01-22 16:41:40 +0100

    [sinhf] fixed spurious divbyzero exception for x=+/-inf

--- src/binary32/sinh/sinhf.c
+++ src/binary32/sinh/sinhf.c
@@ -98,7 +98,7 @@ float cr_sinhf(float x){
     float sgn = __builtin_copysignf(2.0f, x);
     if(ux>=0xff000000u) {
       if(ux<<8) return x + x; // nan
-      return sgn*1.0f/0.0f; // +-inf
+      return x; // +-inf
     }
     float r = sgn*0x1.fffffep127f;
 #ifdef CORE_MATH_SUPPORT_ERRNO
