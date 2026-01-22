cd52504c4eb7350d1c13ad358e3afd6443738ebf
Alexei Sibidanov <sibid@uvic.ca>
2026-01-22 12:16:47 -0800

    [powf] fix for "Spurious divbyzero exception for x=0x1p-149 y=-inf (z=inf)"

--- src/binary32/pow/powf.c
+++ src/binary32/pow/powf.c
@@ -416,7 +416,7 @@ float cr_powf(float x0, float y0){
       if(((tx.u<<1) < ((uint64_t)0x3ff<<53)) ^ (ty.u>>63)){
 	return 0;
       } else {
-	return 1.0f/0.0f;
+	return __builtin_inff();
       }
     }
     return x0 + y0;
