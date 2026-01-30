24b68f1bb11d5afb374a19b1f64ef13b6c882ee9
johnmather@sidefx.com <johnmather@sidefx.com>
2026-01-30 14:02:02 -0500

    Fix asin, asinpi, rsqrt and powl on Windows

--- src/binary80/pow/powl.c
+++ src/binary80/pow/powl.c
@@ -140,7 +140,11 @@ static inline int get_rounding_mode (void)
   /* Warning: on __aarch64__ (for example cfarm103), FE_UPWARD=0x400000
      instead of 0x800. */
 #if defined(__x86_64__) || defined(__arm64__) || defined(_M_ARM64)
-  return _MM_GET_ROUNDING_MODE()>>3;
+  #if defined(__WIN32__) || defined(__WIN64__)
+    return _MM_GET_ROUNDING_MODE()>>5;
+  #else
+    return _MM_GET_ROUNDING_MODE()>>3;
+  #endif
 #else
   return fegetround ();
 #endif
