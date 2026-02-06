d7dbec56f5632421e1bd613cb9dbb92a77ab2847
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2026-02-06 11:41:56 +0100

    [log10p1] fixed further spurious underflow

--- ci/rescale.sh
+++ ci/rescale.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for f in acos acosh acospi asin asinh asinpi atan atanh atanpi cbrt cos cosh cospi erf erfc exp exp10 exp10m1 exp2 exp2m1 expm1 lgamma log log10 log10p1 log1p log2 log2p1 rsqrt sin sinh sinpi support tan tanh tanpi; do
+for f in acos acosh acospi asin asinh asinpi atan atanh atanpi cbrt cos cosh cospi erf erfc exp exp10 exp10m1 exp2 exp2m1 expm1 lgamma log log10 log10p1 log1p log2 log2p1 rsqrt sin sinh sinpi tan tanh tanpi; do
    e=`shuf -i 0-2098 -n 1`
    e=`echo $e - 1074 | bc -q`
    echo Testing $f with rescale $e
