d8892ee7711793499d715836fb8943e3608a7a96
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2026-02-06 11:33:36 +0100

    [log2p1] fixed further spurious underflows

--- src/binary64/log2p1/log2p1.c
+++ src/binary64/log2p1/log2p1.c
@@ -1871,7 +1871,7 @@ cr_log2p1_fast (double *h, double *l, double x, int e, d64u64 v)
   if (e < -5) /* e <= -6 thus |x| < 2^-5 */
   {
     double lo;
-    if (e <= -962)
+    if (e <= -958)
     {
       /* For e <= -969, |x| might be as small as 2^-969, thus h=x/log(2) might
          be in the binade [2^-969,2^-968), with ulp(h) = 2^-1021, and if
@@ -1879,7 +1879,9 @@ cr_log2p1_fast (double *h, double *l, double x, int e, d64u64 v)
          that case to the accurate path.
          Moreover, for e <= -962, the computation 0x1.d4p-62 * *h of the
          error term below might produce a spurious underflow. We also defer
-         that case to the accurate path. */
+         that case to the accurate path.
+         In addition some spurious underflow might happen in the d_mul()
+         call. */
       *h = *l = 0;
       return 1;
     }
