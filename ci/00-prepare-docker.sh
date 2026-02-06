bf6b7b519eeee2b231babf7ecf93dab5cb8bc972
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2026-02-06 15:22:25 +0100

    [ci] bc is needed for rescale

--- ci/00-prepare-docker.sh
+++ ci/00-prepare-docker.sh
@@ -4,7 +4,7 @@
 # barely usable. And anyway we don't care: there's no software that         
 # we want to pull from these repos anyway.                                  
 find /etc/apt/sources.list.d/ -type f | xargs -r grep -li intel | xargs -r rm
-apt-get update -qq && apt-get install -qq build-essential git libmpfr-dev autoconf libtool texinfo
+apt-get update -qq && apt-get install -qq build-essential git libmpfr-dev autoconf libtool texinfo bc
 # the default MPFR library does not contain new C23 functions
 # we thus install the development version of MPFR
 git clone https://gitlab.inria.fr/mpfr/mpfr.git
