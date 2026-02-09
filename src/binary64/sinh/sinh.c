3da0e17da70807dd0f4a47a3a58970f4c154ffc9
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2026-02-09 14:23:30 +0100

    [sinh] more exhaustive tests

--- src/binary64/sinh/sinh.c
+++ src/binary64/sinh/sinh.c
@@ -305,7 +305,7 @@ double cr_sinh(double x){
   }
   // now 0.25 <= |x| < 710.47586
   /* checked exhaustively with/without FMA:
-   * 0.25 <= x < 0.5
+   * 0.25 <= x < 1
    */
   int64_t il = ((u64)jt.u<<14)>>40, jl = -il;
   int64_t i1 = il&0x3f, i0 = (il>>6)&0x3f, ie = il>>12;
