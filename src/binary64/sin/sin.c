436f0400d9d8c11ba16fb13332b9a87e62b41d2c
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2026-02-20 11:23:30 +0100

    [sin] fixed spurious invalid with qnan and non-zero payload

--- src/binary64/sin/sin.c
+++ src/binary64/sin/sin.c
@@ -2034,17 +2034,13 @@ cr_sin (double x)
 
   if (__builtin_expect (e == 0x7ff, 0)) /* NaN, +Inf and -Inf. */
     {
+      if ((t.u << 1) == 0x7ffull<<53){ // +/-Inf
 #ifdef CORE_MATH_SUPPORT_ERRNO
-      if ((t.u << 1) == 0x7ffull<<53){ // Inf
         errno = EDOM;
-        return 0.0 / 0.0;
-      }
 #endif
-      if ((t.u << 1) != 0x7ff8ull<<49){
         return 0.0 / 0.0;
       }
-      t.u = ~0ull;
-      return t.f;
+      return x + x;
     }
 
   /* now x is a regular number */
