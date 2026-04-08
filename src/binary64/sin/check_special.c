4fd982a247d082f413b5e7c71ab62a64e6d087a2
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2026-04-08 14:26:42 +0200

    [sin] reduce number of special checks

--- src/binary64/sin/check_special.c
+++ src/binary64/sin/check_special.c
@@ -258,7 +258,7 @@ main (int argc, char *argv[])
   check_invalid ();
 
 #ifndef CORE_MATH_TESTS
-#define CORE_MATH_TESTS 1000000000UL /* total number of tests */
+#define CORE_MATH_TESTS 500000000UL /* total number of tests */
 #endif
 
   unsigned int seed = getpid ();
