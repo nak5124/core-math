353faee4cd8202fbb88fe61f4f8df5f4822033d1
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2026-04-13 11:25:44 +0200

    [asin] increase error bound for |x|>0.5

--- src/binary64/asin/asin.c
+++ src/binary64/asin/asin.c
@@ -218,8 +218,8 @@ double cr_asin(double x){
     z = __builtin_copysign(__builtin_sqrt(t), -x);
     zl = __builtin_fma(z,z,-t)*((-0.5/t)*z);
     t = 0.25*t - jd*0x1p-7;
-    // fails with 0x1.33p-52 and x=0x1.1564000a6f587p-1 (rndz)
-    eps = __builtin_fabs(z*t)*0x1.34p-52;
+    // fails with 0x1.41p-52 and x=0x1.158155c9abda4p-1 (rndz)
+    eps = __builtin_fabs(z*t)*0x1.42p-52;
   } else { // |x|<=0.5
     // for |x| < 0x1.7137449123ef6p-26 |asin(x) - x| is less than half of ulp of asin(x)
     if(__builtin_expect(ax<0x7cae26e892247decull, 0)) return __builtin_fma(0x1p-55,x,x);
