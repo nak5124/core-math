573684438cb7575ae8f3d7cebf0d42a7d56ac702
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2026-04-23 11:17:46 +0200

    [tgamma] allow XMIN,XMAX

--- src/generic/tgamma/random_under_test.h
+++ src/generic/tgamma/random_under_test.h
@@ -1,5 +1,13 @@
+#ifndef XMAX
+#define XMAX 20
+#endif
+
+#ifndef XMIN
+#define XMIN -XMAX
+#endif
+
 static inline TYPE_UNDER_TEST random_under_test (void)
 {
   /* sample in [-20,20] */
-  return -40. * ((double) rand() / (double) RAND_MAX) + 20;
+  return XMIN + (XMAX - XMIN) * ((double) rand() / (double) RAND_MAX);
 }
